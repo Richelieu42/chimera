f7d6331650f47db9b9595b26a12d5f2844b42bfa
Richelieu <richelieu042@gmail.com>
2024-04-02 17:17:51 +0800

    dev

--- src/component/mq/pulsarKit/producer.go
+++ src/component/mq/pulsarKit/producer.go
@@ -33,56 +33,53 @@ func (p *Producer) Close() {
 /*
 PS: 目标Pulsar服务未启动的情况下，如果ctx不加以限制，要过约 1min 才会返回error（期间客户端日志有connection refused输出）.
 
+@param ctx				建议设置超时时间，否则可能卡死（addresses无效的情况下）
 @param options 			必须的属性: Topic
 						建议的属性: SendTimeout
 @param clientLogPath 	客户端的日志输出（为空则输出到控制台; 不会rotate）
 */
-func NewProducerOriginally(ctx context.Context, addresses []string, options pulsar.ProducerOptions, clientLogPath string) (*Producer, error) {
-	var client pulsar.Client
-	var producer pulsar.Producer
+func NewProducerOriginally(ctx context.Context, addresses []string, options pulsar.ProducerOptions, clientLogPath string) (rst *Producer, err error) {
+	rst = &Producer{
+		Client:   nil,
+		Producer: nil,
+	}
+	defer func() {
+		if err != nil {
+			err = errorKit.Wrapf(err, "fail to new producer")
+		}
+	}()
 
 	// 写入nil: 新建Producer成功
 	errCh := make(chan error, 1)
 
 	go func() {
 		var err error
-		client, err = NewClient(addresses, clientLogPath)
+
+		rst.Client, err = NewClient(addresses, clientLogPath)
 		if err != nil {
 			errCh <- err
 			return
 		}
-		producer, err = client.CreateProducer(options)
+
+		rst.Producer, err = rst.Client.CreateProducer(options)
 		if err != nil {
 			err = errorKit.Wrapf(err, "client fails to create producer")
 			errCh <- err
 			return
 		}
 
-		select {
-		case <-ctx.Done():
-			// 新建Producer成功之前，ctx已经 超时 或 被取消 了，此时需要释放资源
-			if producer != nil {
-				producer.Close()
-			}
-			if client != nil {
-				client.Close()
-			}
-			errCh <- ctx.Err()
-		default:
-			errCh <- nil
-		}
+		errCh <- nil
 	}()
 
 	select {
 	case <-ctx.Done():
+		rst.Close()
 		return nil, ctx.Err()
 	case err := <-errCh:
 		if err != nil {
+			rst.Close()
 			return nil, err
 		}
-		return &Producer{
-			Client:   client,
-			Producer: producer,
-		}, nil
+		return rst, nil
 	}
 }
