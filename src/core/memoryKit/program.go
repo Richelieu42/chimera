2555aa2874cd7dfec6ae0419d3e433af1d045b7a
Richelieu <richelieu042@gmail.com>
2024-04-02 11:11:47 +0800

    dev

--- src/core/memoryKit/program.go
+++ src/core/memoryKit/program.go
@@ -9,15 +9,15 @@ import (
 runtime.MemStats结构体的的字段:
 (1) Alloc（单位: 字节）
 	Alloc is bytes of allocated heap objects.
-	分配的堆对象的字节数。
+	当前分配给 heap 的内存量（不包括尚未释放的对象）.
 
 (2) TotalAlloc（单位: 字节）
 	TotalAlloc is cumulative bytes allocated for heap objects.
-	堆对象分配的累积字节数。与 Alloc 不同，当对象被释放时它不会减少。
+	自程序启动以来，堆上分配的总内存量.
 
 (3) Sys（单位: 字节）
 	Sys is the total bytes of memory obtained from the OS.
-	从操作系统获得的内存总字节数。这测量了 Go 运行时为堆、栈和其他内部数据结构保留的虚拟地址空间。
+	程序向操作系统申请的总内存量（包括未使用的页）.
 
 (4) NumGC
 	NumGC is the number of completed GC cycles.
@@ -33,7 +33,7 @@ runtime.MemStats结构体的的字段:
 	表示是否启用调试垃圾回收。它是一个 bool 类型，如果为 true，则启用调试垃圾回收；如果为 false，则禁用调试垃圾回收。
 */
 func GetProgramMemoryStats() *runtime.MemStats {
-	var stats = &runtime.MemStats{}
+	stats := &runtime.MemStats{}
 	runtime.ReadMemStats(stats)
 	return stats
 }
--- test/test3.go
+++ test/test3.go
@@ -1,8 +1,11 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"github.com/richelieu-yang/chimera/v3/src/core/ioKit"
+	"github.com/richelieu-yang/chimera/v3/src/core/memoryKit"
+	"github.com/richelieu-yang/chimera/v3/src/dataSizeKit"
 	"github.com/richelieu-yang/chimera/v3/src/file/fileKit"
 	_ "github.com/richelieu-yang/chimera/v3/src/log/logrusInitKit"
 	"io"
@@ -10,6 +13,18 @@ import (
 )
 
 func main() {
+	go func() {
+		for {
+			time.Sleep(time.Millisecond * 200)
+			stats := memoryKit.GetProgramMemoryStats()
+
+			fmt.Println(dataSizeKit.ToReadableIecString(float64(stats.Alloc)))
+			fmt.Println(dataSizeKit.ToReadableIecString(float64(stats.TotalAlloc)))
+			fmt.Println(dataSizeKit.ToReadableIecString(float64(stats.Sys)))
+			fmt.Println("---")
+		}
+	}()
+
 	path := "/Users/richelieu/Documents/ino.7z"
 
 	f, err := fileKit.Open(path)
@@ -20,8 +35,8 @@ func main() {
 	start := time.Now()
 
 	var reader io.Reader
-	reader = f
-	//reader = bufio.NewReader(f)
+	//reader = f
+	reader = bufio.NewReader(f)
 	_, err = ioKit.ReadAll(reader)
 	if err != nil {
 		panic(err)
