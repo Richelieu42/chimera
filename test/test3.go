a4111bfa0f167df1313c5e01c78010c2c05edbfc
Richelieu <richelieu042@gmail.com>
2024-04-03 13:55:29 +0800

    dev

--- test/test3.go
+++ test/test3.go
@@ -2,13 +2,19 @@ package main
 
 import (
 	"fmt"
+	"github.com/duke-git/lancet/v2/fileutil"
 	_ "github.com/richelieu-yang/chimera/v3/src/log/logrusInitKit"
 	"github.com/richelieu-yang/chimera/v3/src/office/excelKit"
 )
 
 func main() {
-	fmt.Println(excelKit.JoinCellName("C", 3)) // C3 <nil>
+	path := "/Users/richelieu/Desktop/未命名.xlsx"
+	f, err := excelKit.NewFileWithPath(path)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(f.Close())
 
-	fmt.Println(excelKit.SplitCellName("C3")) // C 3 <nil>
-	fmt.Println(excelKit.SplitCellName("c3")) // c 3 <nil>
+	fileutil.FileSize()
+
 }
