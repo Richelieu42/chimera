99e3dc815cfa3786c884167438f2b6df1f41135b
Richelieu <richelieu042@gmail.com>
2024-04-03 14:26:56 +0800

    dev

--- src/office/excelKit/cell_value.go
+++ src/office/excelKit/cell_value.go
@@ -15,3 +15,35 @@ func SetCellValue(f *excelize.File, sheetName string, col, row int, value interf
 	}
 	return f.SetCellValue(sheetName, cellName, value)
 }
+
+func SetCellBool(f *excelize.File, sheetName string, col, row int, value bool) error {
+	cellName, err := CoordinatesToCellName(col, row)
+	if err != nil {
+		return err
+	}
+	return f.SetCellBool(sheetName, cellName, value)
+}
+
+func SetCellInt(f *excelize.File, sheetName string, col, row int, value int) error {
+	cellName, err := CoordinatesToCellName(col, row)
+	if err != nil {
+		return err
+	}
+	return f.SetCellInt(sheetName, cellName, value)
+}
+
+func SetCellFloat(f *excelize.File, sheetName string, col, row int, value float64, precision, bitSize int) error {
+	cellName, err := CoordinatesToCellName(col, row)
+	if err != nil {
+		return err
+	}
+	return f.SetCellFloat(sheetName, cellName, value, precision, bitSize)
+}
+
+func SetCellStr(f *excelize.File, sheetName string, col, row int, value string) error {
+	cellName, err := CoordinatesToCellName(col, row)
+	if err != nil {
+		return err
+	}
+	return f.SetCellStr(sheetName, cellName, value)
+}
