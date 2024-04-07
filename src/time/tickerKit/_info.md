e6673b689ee89817f527e37feead5567bdca76c1
Richelieu <richelieu042@gmail.com>
2024-04-07 13:42:08 +0800

    dev

--- src/time/tickerKit/_info.md
+++ src/time/tickerKit/_info.md
@@ -1,5 +1,8 @@
 ## Ticker.Stop()
 - Stop turns off a ticker. After Stop, no more ticks will be sent.
-- Stop does not close the channel, to prevent a concurrent goroutine reading from the channel from seeing an erroneous "tick".
+- Stop does not close the channel, to prevent a concurrent goroutine reading
+  from the channel from seeing an erroneous "tick".
+- 可以重复调用Stop方法.
+
 
 
--- src/time/tickerKit/ticker_test.go
+++ src/time/tickerKit/ticker_test.go
@@ -29,3 +29,30 @@ func TestNewTicker(t *testing.T) {
 
 	time.Sleep(time.Second * 3)
 }
+
+// 可以重复调用Stop方法.
+func TestNewTicker1(t *testing.T) {
+	// 创建一个每秒触发一次的Ticker
+	ticker := NewTicker(time.Second)
+
+	/*
+		使用for循环持续接收并处理Tick事件
+		!!!: 缺陷：虽然调用了Stop方法，但是 ticker.C通道 并没有关闭，导致goroutine无法退出.
+	*/
+	go func() {
+		defer fmt.Println("goroutine ends")
+
+		for t := range ticker.C {
+			fmt.Println("Tick at", t)
+		}
+	}()
+
+	// 等待 5s 后停止Ticker
+	time.Sleep(5 * time.Second)
+	ticker.Stop()
+	ticker.Stop()
+	ticker.Stop()
+	fmt.Println("Ticker stopped.")
+
+	time.Sleep(time.Second * 3)
+}
