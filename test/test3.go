41b166ff20cf1708ec006cb9217cc0708853b4c1
Richelieu <richelieu042@gmail.com>
2024-04-09 13:27:04 +0800

    dev

--- test/test3.go
+++ test/test3.go
@@ -1,27 +1,32 @@
 package main
 
 import (
-	"github.com/gin-contrib/static"
-	"github.com/gin-gonic/gin"
-	"github.com/richelieu-yang/chimera/v3/src/component/web/ginKit"
+	"embed"
+	"fmt"
+	"io"
 )
 
+//go:embed c
+var a embed.FS
+
+//go:embed c/a.json
+var b []byte
+
+//go:embed c/a.json
+var c string
+
 func main() {
-	r := gin.Default()
+	f, err := a.Open("c/a.json")
+	if err != nil {
+		panic(err)
+	}
+	data, err := io.ReadAll(f)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(string(data))
 
-	// if Allow DirectoryIndex
-	//r.Use(static.Serve("/", static.LocalFile("/tmp", true)))
-	// set prefix
-	//r.Use(static.Serve("/static", static.LocalFile("/tmp", true)))
+	fmt.Println(string(b))
 
-	r.Use(static.Serve("/", static.LocalFile("/tmp", false)))
-
-	r.GET("/ping", func(c *gin.Context) {
-		c.String(200, "test")
-	})
-
-	ginKit.StaticDir()
-
-	// Listen and Server in 0.0.0.0:8080
-	r.Run(":8080")
+	fmt.Println(c)
 }
