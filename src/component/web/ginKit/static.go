1734ae4770a0aac44f551aeafd1c6a3428a5a535
Richelieu <richelieu042@gmail.com>
2024-04-09 09:30:24 +0800

    dev

--- src/component/web/ginKit/static.go
+++ src/component/web/ginKit/static.go
@@ -39,6 +39,8 @@ func StaticFile(group IGroup, relativePath, filePath string) error {
 
 // StaticDir 静态资源（目录）
 /*
+PS: 适用场景: 静态资源与已绑定路由不冲突的情况（如果冲突的话，建议使用 NewStaticMiddleware）.
+
 @param relativePath		路由
 @param root				相对路径（对于项目的根目录(working directory)，而非main()所在的目录（虽然他们常常是同一个）） || 绝对路径
 @param listDirectory 	是否列出目录下的文件，true: 当目录下不存 index.html 文件时，会列出该目录下的所有文件（正式环境不推荐，因为不安全）
