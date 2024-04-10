3fa9d57f3210ba622a9de42d843d33812456671a
Richelieu <richelieu042@gmail.com>
2024-04-10 13:15:34 +0800

    dev

--- src/time/timeKit/_info.md
+++ src/time/timeKit/_info.md
@@ -1,10 +1,14 @@
 ## 参考
-cron相关可以参考:
-    定时任务-gcron https://goframe.org/pages/viewpage.action?pageId=1114187
+- [定时任务-gcron](https://goframe.org/pages/viewpage.action?pageId=1114187)
+- notes/Golang/Golang.wps
 
+## 时间参照标准（时区）
+PS:  
+(1) UTC 和 GMT 可以看做是一样的;   
+(2) GMT 现已非正式标准.  
 
-
-
-
+- UTC: 世界协调时间(Coordinated Universal Time)
+- GMT: 格林威治时间，GMT = UTC+0
+- CST: 中国标准时间(China Standard Time)，CST = UTC+8
 
 
--- src/time/timeKit/format.go
+++ src/time/timeKit/format.go
@@ -5,6 +5,45 @@ import (
 	"time"
 )
 
+type (
+	TimeFormat string
+)
+
+const (
+	FormatDate TimeFormat = "2006-01-02"
+
+	// FormatDefault 参考 time/format.go
+	FormatDefault TimeFormat = "2006-01-02 15:04:05.999999999 -0700 MST"
+
+	// FormatFileName 用于作为文件名（或目录名）的一部分
+	/*
+		PS:
+		(1) 不能使用 "2006-01-02T15-04-05-000".
+		(2) Windows OS，文件名不支持: \ / : * ? " < > |
+	*/
+	FormatFileName TimeFormat = "2006-01-02T15.04.05.000"
+
+	// FormatCommon 常规的格式
+	FormatCommon TimeFormat = "2006-01-02T15:04:05.000"
+	// FormatCommon1 常规的格式1
+	FormatCommon1 TimeFormat = "2006-01-02 15:04:05.000"
+
+	// FormatEntire 完整的格式
+	FormatEntire  TimeFormat = "2006-01-02 15:04:05.000Z07:00 MST"
+	FormatEntire1 TimeFormat = "2006-01-02T15:04:05.000Z07:00 MST"
+	FormatEntire2 TimeFormat = "2006-01-02T15:04:05.000Z07:00"
+
+	// FormatNetwork 网络的格式
+	FormatNetwork TimeFormat = "Mon, 02 Jan 2006 15:04:05 MST"
+
+	// FormatRFC1123 网络的格式
+	FormatRFC1123 TimeFormat = time.RFC1123
+
+	FormatA TimeFormat = "2006-01-02 15:04:05"
+	FormatB TimeFormat = "2006-01-02 3:04:05.000 PM Mon Jan"
+	FormatC TimeFormat = "3:04:05.000 PM Mon Jan"
+)
+
 // FormatCurrent 格式化 当前时间 为 字符串.
 /*
 @param format 可以为""
