4894ab2850b524592f420a386b418c98f9923168
Richelieu <richelieu042@gmail.com>
2024-04-10 14:49:18 +0800

    dev

--- src/component/web/ginKit/route_favicon.go
+++ src/component/web/ginKit/route_favicon.go
@@ -2,6 +2,7 @@ package ginKit
 
 import (
 	"github.com/gin-gonic/gin"
+	"github.com/richelieu-yang/chimera/v3/src/component/web/httpKit"
 	"net/http"
 )
 
@@ -9,6 +10,8 @@ func DefaultFavicon(engine *gin.Engine) {
 	httpFileSystem := http.FS(efs)
 
 	engine.GET("/favicon.ico", func(ctx *gin.Context) {
+		// 缓存24h
+		SetResponseHeader(ctx, httpKit.HeaderCacheControl, "public, max-age=86400, must-revalidate")
 		ctx.FileFromFS("_icon/favicon.ico", httpFileSystem)
 	})
 }
