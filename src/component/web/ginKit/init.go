24d3edcc486f122b564d000dce80115acce3ec2b
Richelieu <richelieu042@gmail.com>
2024-04-10 09:25:45 +0800

    dev

---
+++ src/component/web/ginKit/init.go
@@ -0,0 +1,6 @@
+package ginKit
+
+import "embed"
+
+//go:embed _html/* _icon/*
+var efs embed.FS
--- src/component/web/ginKit/route_favicon.go
+++ src/component/web/ginKit/route_favicon.go
@@ -2,14 +2,16 @@ package ginKit
 
 import (
 	"github.com/gin-gonic/gin"
-	"github.com/richelieu-yang/chimera/v3/internal/resources"
 )
 
 func DefaultFavicon(engine *gin.Engine) {
-	path := "_resources/icon/favicon.ico"
-	fs := resources.AssetFile()
+	//path := "_resources/icon/favicon.ico"
+	//fs := resources.AssetFile()
 
 	engine.GET("/favicon.ico", func(ctx *gin.Context) {
-		ctx.FileFromFS(path, fs)
+
+		efs.Open("_icon/favicon.ico")
+
+		//ctx.FileFromFS(path, fs)
 	})
 }
