d4847d894929cd93202188bd092463ba5f7cdd7a
Richelieu <richelieu042@gmail.com>
2024-04-11 14:25:39 +0800

    dev

--- test/test3.go
+++ test/test3.go
@@ -1,11 +1,9 @@
 package main
 
 import (
-	"fmt"
 	_ "github.com/richelieu-yang/chimera/v3/src/log/logrusInitKit"
-	"github.com/sirupsen/logrus"
 )
 
 func main() {
-	fmt.Println(logrus.StandardLogger().Level.String())
+
 }
