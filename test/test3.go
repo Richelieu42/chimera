ec9f6ae64012729d4286cdf45f312b4e93c318c3
Richelieu <richelieu042@gmail.com>
2024-04-11 17:08:45 +0800

    dev

--- test/test3.go
+++ test/test3.go
@@ -1,11 +1,5 @@
 package main
 
-import (
-	"fmt"
-	"github.com/richelieu-yang/chimera/v3/src/dataSizeKit"
-	_ "github.com/richelieu-yang/chimera/v3/src/log/logrusInitKit"
-)
-
 func main() {
-	fmt.Println(dataSizeKit.ToReadableIecString(31916))
+
 }
