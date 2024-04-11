b8167d5c9d5bac14acef6b20544def00cdfd2de6
Richelieu <richelieu042@gmail.com>
2024-04-11 14:37:24 +0800

    dev

--- src/log/logrusKit/hook_lfshook.go
+++ src/log/logrusKit/hook_lfshook.go
@@ -5,6 +5,9 @@ import (
 	"github.com/sirupsen/logrus"
 )
 
-func NewLfsHook(output interface{}, formatter logrus.Formatter) *lfshook.LfsHook {
-	return lfshook.NewHook(output, formatter)
-}
+// NewLfsHook
+/*
+使用场景:
+(1) 指定日志级别(s)，可以也输出到其它输出，不影响原输出.
+*/
+var NewLfsHook func(output interface{}, formatter logrus.Formatter) *lfshook.LfsHook = lfshook.NewHook
