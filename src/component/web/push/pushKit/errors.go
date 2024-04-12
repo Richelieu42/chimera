f42b8fcc2d08deff84ec725377422e78ef9aa8ac
Richelieu <richelieu042@gmail.com>
2024-04-12 14:38:09 +0800

    dev

--- src/component/web/push/pushKit/errors.go
+++ src/component/web/push/pushKit/errors.go
@@ -5,5 +5,7 @@ import "github.com/richelieu-yang/chimera/v3/src/core/errorKit"
 var (
 	NotSetupError = errorKit.Newf("haven’t been set up correctly")
 
-	ChannelClosedError = errorKit.Newf("Channel is already closed")
+	ChannelClosedError = errorKit.Newf("channel has already been closed")
+
+	NoSuitableChannelError = errorKit.Newf("no suitable channel")
 )
