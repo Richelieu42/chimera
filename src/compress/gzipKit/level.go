367fe20ba7ba854dad8b63786a3afba371c24298
Richelieu <richelieu042@gmail.com>
2024-04-12 14:22:28 +0800

    dev

--- src/compress/gzipKit/level.go
+++ src/compress/gzipKit/level.go
@@ -4,6 +4,14 @@ import (
 	"compress/gzip"
 )
 
+const (
+	LevelNoCompression = gzip.NoCompression
+
+	LevelBestSpeed = gzip.BestSpeed
+
+	LevelBestCompression = gzip.BestCompression
+)
+
 // IsValidLevel
 /*
 @param level 有效范围: [-2, 9]
