eea45faee16b82d9eedc08f983462f7ac5c46313
Richelieu <richelieu042@gmail.com>
2024-04-15 10:57:39 +0800

    dev

--- src/i18n/i18nPackKit/setup.go
+++ src/i18n/i18nPackKit/setup.go
@@ -1,7 +1,6 @@
 package i18nPackKit
 
 import (
-	"github.com/duke-git/lancet/v2/condition"
 	"github.com/nicksnyder/go-i18n/v2/i18n"
 	"github.com/richelieu-yang/chimera/v3/src/core/errorKit"
 	"github.com/richelieu-yang/chimera/v3/src/i18n/i18nKit"
@@ -13,7 +12,8 @@ var (
 )
 
 var (
-	innerBundle    *i18n.Bundle
+	innerBundle *i18n.Bundle
+
 	innerBeanMaker BeanMaker
 
 	defaultBeanMaker = func(code, msg string, data interface{}) interface{} {
@@ -25,11 +25,18 @@ var (
 	}
 )
 
+// SetUp
+/*
+@param maker 可以为nil
+*/
 func SetUp(defaultLanguage language.Tag, maker BeanMaker) {
 	innerBundle = i18nKit.NewBundle(defaultLanguage)
 	innerBeanMaker = maker
 }
 
 func getMaker() BeanMaker {
-	return condition.TernaryOperator(innerBeanMaker != nil, innerBeanMaker, defaultBeanMaker)
+	if innerBeanMaker != nil {
+		return innerBeanMaker
+	}
+	return defaultBeanMaker
 }
