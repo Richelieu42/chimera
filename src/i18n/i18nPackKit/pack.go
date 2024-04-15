74b970a48a0930846a570d740cbbd27f85b256d7
Richelieu <richelieu042@gmail.com>
2024-04-15 10:55:20 +0800

    dev

--- src/i18n/i18nPackKit/pack.go
+++ src/i18n/i18nPackKit/pack.go
@@ -6,10 +6,18 @@ import (
 	"github.com/richelieu-yang/chimera/v3/src/i18n/i18nKit"
 )
 
+// Pack
+/*
+@param langs 可以为空（包括nil）
+*/
 func Pack(langs []string, code string, data interface{}, msgArgs ...interface{}) interface{} {
 	return PackFully(langs, code, "", data, msgArgs...)
 }
 
+// PackFully
+/*
+@param langs 可以为空（包括nil）
+*/
 func PackFully(langs []string, code, msg string, data interface{}, msgArgs ...interface{}) interface{} {
 	var err error
 
--- src/i18n/i18nPackKit/seal.go
+++ src/i18n/i18nPackKit/seal.go
@@ -2,11 +2,19 @@ package i18nPackKit
 
 import "github.com/richelieu-yang/chimera/v3/src/serialize/json/jsonKit"
 
+// Seal
+/*
+@param langs 可以为空（包括nil）
+*/
 func Seal(langs []string, code string, data interface{}, msgArgs ...interface{}) (string, error) {
 	bean := Pack(langs, code, data, msgArgs...)
 	return jsonKit.MarshalToString(bean)
 }
 
+// SealFully
+/*
+@param langs 可以为空（包括nil）
+*/
 func SealFully(langs []string, code, msg string, data interface{}, msgArgs ...interface{}) (string, error) {
 	bean := PackFully(langs, code, msg, data, msgArgs...)
 	return jsonKit.MarshalToString(bean)
