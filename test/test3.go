c8baaecef5a0b42ffea82e45bfc1fd72b2ff3a78
Richelieu <richelieu042@gmail.com>
2024-05-04 13:48:12 +0800

    dev

--- test/test3.go
+++ test/test3.go
@@ -1,14 +1,5 @@
 package main
 
-import (
-	"github.com/centrifugal/gocent/v3"
-)
-
 func main() {
-	client := gocent.New(gocent.Config{
-		Addr:       "",
-		GetAddr:    nil,
-		Key:        "",
-		HTTPClient: nil,
-	})
+
 }
