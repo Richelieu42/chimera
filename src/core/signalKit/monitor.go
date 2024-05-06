a26acd96d32db76156d9ad6ce21f2eca6255e061
Richelieu <richelieu042@gmail.com>
2024-05-06 13:43:23 +0800

    dev

--- src/core/signalKit/monitor.go
+++ src/core/signalKit/monitor.go
@@ -8,13 +8,12 @@ import (
 	"time"
 )
 
+var printOnce sync.Once
 var exitOnce sync.Once
 
 // MonitorExitSignals 异步地监听退出信号（拦截关闭信号）.
 /*
 可以参考 go-zero 中的 "core/proc/signals.go".
-
-@param callbacks 可以不传
 
 PS:
 (1) 无法拦截部分信号（e.g. syscall.SIGSTOP、syscall.SIGKILL）;
@@ -23,32 +22,43 @@ PS:
 (4) 信号处理函数中不要使用 fmt.Println 等函数，因为它们不是线程安全的，会导致程序崩溃;
 (5) 虽然可以多次调用本函数，但不推荐这么干，1次就够了.
 */
-func MonitorExitSignals(callbacks ...func(sig os.Signal)) {
+func MonitorExitSignals(callback func(sig os.Signal)) {
 	ch := make(chan os.Signal, 1)
 	signal.Notify(ch, ExitSignals...)
 
 	go func() {
 		sig := <-ch
-		for _, callback := range callbacks {
-			callback(sig)
-		}
+		printOnce.Do(func() {
+			logrus.Warnf("Receive an exit signal(%s).", sig.String())
+		})
 
+		callback(sig)
 		exitOnce.Do(func() {
 			time.Sleep(time.Second * 3)
 
-			logrus.Fatalf("Receive an exit signal(%s).", sig.String())
+			logrus.Fatalf("Process exits with signal(%s).", sig.String())
 		})
 	}()
 }
 
 // MonitorExitSignalsSynchronously 同步地（会阻塞调用的goroutine）
-func MonitorExitSignalsSynchronously(callbacks ...func(sig os.Signal)) {
+func MonitorExitSignalsSynchronously(callback func(sig os.Signal)) {
 	ch := make(chan os.Signal, 1)
 	signal.Notify(ch, ExitSignals...)
 
 	sig := <-ch
-	for _, callback := range callbacks {
-		callback(sig)
-	}
-	logrus.WithField("signal", sig.String()).Fatal("Receive an exit signal.")
+	printOnce.Do(func() {
+		logrus.Warnf("Receive an exit signal(%s).", sig.String())
+	})
+
+	callback(sig)
+	exitOnce.Do(func() {
+		time.Sleep(time.Second * 3)
+
+		exitOnce.Do(func() {
+			time.Sleep(time.Second * 3)
+
+			logrus.Fatalf("Process exits with signal(%s).", sig.String())
+		})
+	})
 }
--- test/test3.go
+++ test/test3.go
@@ -1,5 +1,26 @@
 package main
 
-func main() {
+import (
+	_ "github.com/richelieu-yang/chimera/v3/src/log/logrusInitKit"
+	"github.com/sirupsen/logrus"
+	"time"
 
+	"github.com/richelieu-yang/chimera/v3/src/core/signalKit"
+	"os"
+)
+
+func main() {
+	signalKit.MonitorExitSignals(func(sig os.Signal) {
+		time.Sleep(time.Second)
+		logrus.Info(sig.String())
+	})
+	signalKit.MonitorExitSignals(func(sig os.Signal) {
+		time.Sleep(time.Second)
+		logrus.Info(sig.String())
+	})
+	signalKit.MonitorExitSignals(func(sig os.Signal) {
+		time.Sleep(time.Second)
+		logrus.Info(sig.String())
+	})
+	select {}
 }
