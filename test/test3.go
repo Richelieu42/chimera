f2c3877993c464c7c7d0b3a21e8a2afe5a00a1c3
Richelieu <richelieu042@gmail.com>
2024-05-07 11:27:37 +0800

    dev

--- test/test3.go
+++ test/test3.go
@@ -1,32 +1,5 @@
 package main
 
-import (
-	"context"
-	"errors"
-	"fmt"
-	_ "github.com/richelieu-yang/chimera/v3/src/log/logrusInitKit"
-)
-
 func main() {
-	ctx := context.TODO()
 
-	fmt.Println(ctx.Err() == nil)                               // true
-	fmt.Println(errors.Is(ctx.Err(), context.Canceled))         // false
-	fmt.Println(errors.Is(ctx.Err(), context.DeadlineExceeded)) // false
 }
-
-//func main() {
-//	//signalKit.MonitorExitSignals(func(sig os.Signal) {
-//	//	time.Sleep(time.Second)
-//	//	logrus.Info(sig.String())
-//	//})
-//	//signalKit.MonitorExitSignals(func(sig os.Signal) {
-//	//	time.Sleep(time.Second)
-//	//	logrus.Info(sig.String())
-//	//})
-//	//signalKit.MonitorExitSignals(func(sig os.Signal) {
-//	//	time.Sleep(time.Second)
-//	//	logrus.Info(sig.String())
-//	//})
-//	//select {}
-//}
