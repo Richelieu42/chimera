42476647034e6d3b3301592730a3cb981b1fc18e
Richelieu <richelieu042@gmail.com>
2024-05-14 09:03:56 +0800

    dev

--- src/ip/ipRegionKit/setup_test.go
+++ src/ip/ipRegionKit/setup_test.go
@@ -14,6 +14,7 @@ func TestGetRegion(t *testing.T) {
 	}
 	logrus.Infof("wd: [%s].", wd)
 
+	// https://github.com/lionsoul2014/ip2region/blob/master/data/ip2region.xdb
 	xdbPath := "_chimera-lib/ip2region.xdb"
 	MustSetUp(xdbPath)
 
