22fad0d430e3b44f106fd09e49ecfc4accd931b0
Richelieu <richelieu042@gmail.com>
2024-05-15 16:53:45 +0800

    dev

--- src/component/web/proxyKit/proxy.go
+++ src/component/web/proxyKit/proxy.go
@@ -35,6 +35,9 @@ func ProxyWithGin(ctx *gin.Context, targetAddr string, options ...ProxyOption) e
 
 // ProxyToUrl
 /*
+项目实战：用 Go 创建一个简易负载均衡器
+	https://mp.weixin.qq.com/s/pe0CQa3tdrUmC86OSRBNeg
+
 @param targetUrl 可以是 url.Parse() 的返回值
 */
 func ProxyToUrl(w http.ResponseWriter, r *http.Request, targetUrl *url.URL) (err error) {
