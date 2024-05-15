eb61aa5c9dc4ba686c7176b005d6550d9cf9310d
Richelieu <richelieu042@gmail.com>
2024-05-15 16:46:14 +0800

    dev

--- test/test1/test1.go
+++ test/test1/test1.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+	"fmt"
 	_ "github.com/richelieu-yang/chimera/v3/src/log/logrusInitKit"
+	"github.com/richelieu-yang/chimera/v3/src/urlKit"
 )
 
 func main() {
-
+	fmt.Println(urlKit.EncodeURIComponent(":$"))
 }
