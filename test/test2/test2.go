78acb23757027a08c86ce1b316c4941531ce3082
Richelieu <richelieu042@gmail.com>
2024-05-16 14:49:42 +0800

    dev

--- test/test2/test2.go
+++ test/test2/test2.go
@@ -1,26 +1,5 @@
 package main
 
-import (
-	_ "github.com/richelieu-yang/chimera/v3/src/log/logrusInitKit"
-
-	"fmt"
-	"github.com/gin-gonic/gin"
-	"github.com/sirupsen/logrus"
-	"strconv"
-)
-
-var (
-	port = 9002
-)
-
 func main() {
-	engine := gin.Default()
 
-	engine.Any("/test", func(ctx *gin.Context) {
-		ctx.String(200, strconv.Itoa(port))
-	})
-
-	if err := engine.Run(fmt.Sprintf(":%d", port)); err != nil {
-		logrus.Fatal(err)
-	}
 }
