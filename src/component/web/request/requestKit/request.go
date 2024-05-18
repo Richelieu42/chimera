eb98c538ea43dc4beb7377214a4d43844f136780
Richelieu <richelieu042@gmail.com>
2024-05-18 13:32:07 +0800

    dev

--- src/component/web/request/requestKit/request.go
+++ src/component/web/request/requestKit/request.go
@@ -1,1 +1,17 @@
 package requestKit
+
+import "github.com/imroc/req/v3"
+
+// NewRequest
+/*
+@param client 可以为nil，将采用 defClient
+*/
+func NewRequest(client *req.Client) (r *req.Request) {
+	if client == nil {
+		client = defClient
+	}
+
+	r = client.R()
+
+	return
+}
