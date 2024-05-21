7897ec8902db742ebd47dfff5304b2aa6e703484
Richelieu <richelieu042@gmail.com>
2024-05-21 09:13:12 +0800

    dev

--- go.mod
+++ go.mod
@@ -116,7 +116,7 @@ require (
 	golang.org/x/crypto v0.23.0
 	golang.org/x/exp v0.0.0-20240506185415-9bf2ced13842
 	golang.org/x/image v0.16.0
-	golang.org/x/mobile v0.0.0-20240506190922-a1a533f289d3
+	golang.org/x/mobile v0.0.0-20240520174638-fa72addaaa1b
 	golang.org/x/mod v0.17.0
 	golang.org/x/net v0.25.0
 	golang.org/x/oauth2 v0.20.0
---
+++ src/micro/centrifugoKit/grpc_client.go
@@ -0,0 +1,37 @@
+package centrifugoKit
+
+import (
+	"context"
+	"github.com/richelieu-yang/chimera/v3/src/core/sliceKit"
+	"github.com/sirupsen/logrus"
+)
+
+type GrpcClient struct {
+}
+
+func (client *GrpcClient) Publish(ctx context.Context, channel string, data []byte) error {
+	// TODO
+	logrus.Warn("TODO")
+
+	return nil
+}
+
+func (client *GrpcClient) Broadcast(ctx context.Context, channels []string, data []byte) error {
+	// TODO
+	logrus.Warn("TODO")
+
+	return nil
+}
+
+func NewGrpcClient(hosts []string) (client *GrpcClient, err error) {
+	hosts = sliceKit.Uniq(hosts)
+	hosts = sliceKit.RemoveEmpty(hosts, true)
+	if err = sliceKit.AssertNotEmpty(hosts, "hosts"); err != nil {
+		return
+	}
+
+	// TODO
+	logrus.Warn("TODO")
+	client = &GrpcClient{}
+	return
+}
