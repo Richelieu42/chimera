9c2120f95596539608bda7b5ee6316798c51cdee
Richelieu <richelieu042@gmail.com>
2024-05-23 17:02:22 +0800

    dev

--- test/test.go
+++ test/test.go
@@ -1,33 +1,5 @@
 package main
 
-import (
-	"fmt"
-	"github.com/itchyny/gojq"
-)
-
 func main() {
-	query, err := gojq.Parse(".name")
-	if err != nil {
-		panic(err)
-	}
-	input := map[string]interface{}{
-		"name": "Alice",
-		"age":  30,
-		"city": "Wonderland",
-	}
-	iter := query.Run(input)
-	for {
-		v, ok := iter.Next()
-		if !ok {
-			break
-		}
-		if err, ok := v.(error); ok {
-			fmt.Println("error:", err)
-			return
-		}
-		fmt.Println(v)
-	}
-	/*
-		output: Alice
-	*/
+
 }
