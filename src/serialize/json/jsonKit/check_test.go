60852986a18134459d316ac641b47929fb237279
Richelieu <richelieu042@gmail.com>
2024-05-24 17:25:01 +0800

    dev

--- src/serialize/json/jsonKit/check_test.go
+++ src/serialize/json/jsonKit/check_test.go
@@ -6,9 +6,17 @@ import (
 )
 
 func TestIsJsonString(t *testing.T) {
-	fmt.Println(IsJsonString(""))
-	fmt.Println(IsJsonString("{}"))
-	fmt.Println(IsJsonString("[]"))
-	fmt.Println(IsJsonString("qdqwd"))
-	fmt.Println(IsJsonString("{]"))
+	fmt.Println(IsJsonString(""))      // false
+	fmt.Println(IsJsonString("{}"))    // true
+	fmt.Println(IsJsonString("[]"))    // true
+	fmt.Println(IsJsonString("qdqwd")) // false
+	fmt.Println(IsJsonString("{]"))    // false
+}
+
+func TestIsJson(t *testing.T) {
+	fmt.Println(IsJson([]byte("")))      // false
+	fmt.Println(IsJson([]byte("{}")))    // true
+	fmt.Println(IsJson([]byte("[]")))    // true
+	fmt.Println(IsJson([]byte("qdqwd"))) // false
+	fmt.Println(IsJson([]byte("{]")))    // false
 }
