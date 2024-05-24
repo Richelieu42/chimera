f96149c7ace4133425147f5d8555daab46e37501
Richelieu <richelieu042@gmail.com>
2024-05-24 09:05:13 +0800

    dev

--- src/core/sliceKit/element_type.go
+++ src/core/sliceKit/element_type.go
@@ -1,9 +1,12 @@
 package sliceKit
 
-import "github.com/samber/lo"
+import (
+	"github.com/richelieu-yang/chimera/v3/src/core/errorKit"
+	"github.com/samber/lo"
+)
 import lop "github.com/samber/lo/parallel"
 
-// ConvertElementType 转换切片实例的元素类型
+// ConvertElementType 遍历并修改元素的类型（返回值是一个新的slice实例）.
 /*
 @param string	可以为nil
 @param iteratee 不能为nil，会导致panic: runtime error: invalid memory address or nil pointer dereference
@@ -15,11 +18,29 @@ e.g. []int => []string
 	})
 	fmt.Println(s) // [0x0 0x1 0x2 0x3]
 */
-func ConvertElementType[T any, R any](s []T, iteratee func(item T, index int) R) []R {
-	return lo.Map(s, iteratee)
+func ConvertElementType[T any, R any](collection []T, converter func(item T, index int) R) []R {
+	return lo.Map(collection, converter)
 }
 
-// ConvertElementTypeInParallel 转换切片实例的元素类型
+// ConvertElementTypeE
+/*
+对 lo.Map 就行了拓展.
+*/
+func ConvertElementTypeE[T any, R any](collection []T, converter func(item T, index int) (R, error)) ([]R, error) {
+	result := make([]R, len(collection))
+
+	var err error
+	for i, item := range collection {
+		result[i], err = converter(item, i)
+		if err != nil {
+			return nil, errorKit.Wrapf(err, "fail to convert element(index: %d, value: %v)", i, item)
+		}
+	}
+
+	return result, nil
+}
+
+// ConvertElementTypeInParallel 并发地遍历并修改元素的类型（返回值是一个新的slice实例）.
 /*
 PS: 使用协程更加高效，但要慎用!!!
 
@@ -27,6 +48,6 @@ PS: 使用协程更加高效，但要慎用!!!
 @param iteratee 不能为nil，会导致panic: runtime error: invalid memory address or nil pointer dereference
 @return			必定不为nil（保底空的slice实例）
 */
-func ConvertElementTypeInParallel[T any, R any](s []T, iteratee func(item T, index int) R) []R {
-	return lop.Map(s, iteratee)
+func ConvertElementTypeInParallel[T any, R any](collection []T, iteratee func(item T, index int) R) []R {
+	return lop.Map(collection, iteratee)
 }
---
+++ src/grpcKit/type_resolver_builder.go
@@ -0,0 +1,84 @@
+package grpcKit
+
+import (
+	"github.com/richelieu-yang/chimera/v3/src/core/sliceKit"
+	"github.com/richelieu-yang/chimera/v3/src/core/strKit"
+	"google.golang.org/grpc/resolver"
+)
+
+// NewResolverBuilder
+/*
+@param scheme 协议（不能有大写字符）
+*/
+func NewResolverBuilder(scheme string, hosts []string) (resolver.Builder, error) {
+	if err := strKit.AssertNotEmpty(scheme, "scheme"); err != nil {
+		return nil, err
+	}
+	hosts = sliceKit.RemoveEmpty(hosts, true)
+	hosts = sliceKit.Uniq(hosts)
+	if err := sliceKit.AssertNotEmpty(hosts, "hosts"); err != nil {
+		return nil, err
+	}
+
+	addrs := hostsToAddressSlice(hosts)
+	return &manualResolverBuilder{
+		scheme:    scheme,
+		addresses: addrs,
+	}, nil
+}
+
+type manualResolverBuilder struct {
+	resolver.Builder
+
+	// scheme returns the scheme supported by this resolver.  Scheme is defined
+	// at https://github.com/grpc/grpc/blob/master/doc/naming.md.  The returned
+	// string should not contain uppercase characters, as they will not match
+	// the parsed target's scheme as defined in RFC 3986.
+	scheme    string
+	addresses []resolver.Address
+}
+
+func (builder *manualResolverBuilder) Scheme() string {
+	return builder.scheme
+}
+
+func (builder *manualResolverBuilder) Build(target resolver.Target, cc resolver.ClientConn, opts resolver.BuildOptions) (resolver.Resolver, error) {
+	r := &manualResolver{
+		target:    target,
+		cc:        cc,
+		addresses: builder.addresses,
+	}
+	if err := r.start(); err != nil {
+		return nil, err
+	}
+	return r, nil
+}
+
+type manualResolver struct {
+	resolver.Resolver
+
+	target resolver.Target
+	cc     resolver.ClientConn
+
+	addresses []resolver.Address
+}
+
+func (r *manualResolver) start() error {
+	return r.cc.UpdateState(resolver.State{
+		Addresses: r.addresses,
+	})
+}
+
+func (r *manualResolver) ResolveNow(o resolver.ResolveNowOptions) {
+}
+
+func (r *manualResolver) Close() {
+}
+
+func hostsToAddressSlice(hosts []string) []resolver.Address {
+	return sliceKit.ConvertElementType(hosts, func(item string, index int) resolver.Address {
+		return resolver.Address{
+			Addr: item,
+		}
+	})
+}
