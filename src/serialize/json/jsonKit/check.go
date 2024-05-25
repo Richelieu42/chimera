f1e75e2383042083858cd506798fc26dbe877b89
Richelieu <richelieu042@gmail.com>
2024-05-25 09:18:04 +0800

    dev

--- src/serialize/json/jsonKit/check.go
+++ src/serialize/json/jsonKit/check.go
@@ -2,15 +2,7 @@ package jsonKit
 
 import "github.com/richelieu-yang/chimera/v3/src/core/strKit"
 
-func IsJsonString(str string) (rst bool) {
-	if strKit.StartWith(str, "{") {
-		rst = strKit.EndWith(str, "}")
-	} else if strKit.StartWith(str, "[") {
-		rst = strKit.EndWith(str, "]")
-	}
-	return
-}
-
+// IsJson 简单的检测，并不一定准确.
 func IsJson(data []byte) (rst bool) {
 	length := len(data)
 	if length >= 2 {
@@ -19,6 +11,16 @@ func IsJson(data []byte) (rst bool) {
 		} else if data[0] == '[' {
 			rst = data[length-1] == ']'
 		}
+	}
+	return
+}
+
+// IsJsonString 简单的检测，并不一定准确.
+func IsJsonString(str string) (rst bool) {
+	if strKit.StartWith(str, "{") {
+		rst = strKit.EndWith(str, "}")
+	} else if strKit.StartWith(str, "[") {
+		rst = strKit.EndWith(str, "]")
 	}
 	return
 }
