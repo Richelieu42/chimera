59930c265ca674175b0bb5a2a40a604392250de3
Richelieu <richelieu042@gmail.com>
2024-05-25 09:42:45 +0800

    dev

--- src/micro/centrifugoKit/grpc_client_type.go
+++ src/micro/centrifugoKit/grpc_client_type.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"github.com/richelieu-yang/chimera/v3/src/concurrency/mutexKit"
 	"github.com/richelieu-yang/chimera/v3/src/micro/centrifugoKit/apiproto"
+	"github.com/richelieu-yang/chimera/v3/src/serialize/json/jsonKit"
 	"google.golang.org/grpc"
 )
 
@@ -28,10 +29,14 @@ func (client *GrpcClient) Close() (err error) {
 /*
 @param data 必须是json!!!
 */
-func (client *GrpcClient) PublishSimply(ctx context.Context, channel string, data []byte) (*apiproto.PublishResponse, error) {
+func (client *GrpcClient) PublishSimply(ctx context.Context, channel string, jsonData []byte) (*apiproto.PublishResponse, error) {
+	if err := jsonKit.AssertJson(jsonData, "jsonData"); err != nil {
+		return nil, err
+	}
+
 	in := &apiproto.PublishRequest{
 		Channel: channel,
-		Data:    apiproto.Raw(data),
+		Data:    apiproto.Raw(jsonData),
 	}
 	return client.Publish(ctx, in)
 }
@@ -40,10 +45,14 @@ func (client *GrpcClient) PublishSimply(ctx context.Context, channel string, dat
 /*
 @param data 必须是json!!!
 */
-func (client *GrpcClient) BroadcastSimply(ctx context.Context, channels []string, data []byte) (*apiproto.BroadcastResponse, error) {
+func (client *GrpcClient) BroadcastSimply(ctx context.Context, channels []string, jsonData []byte) (*apiproto.BroadcastResponse, error) {
+	if err := jsonKit.AssertJson(jsonData, "jsonData"); err != nil {
+		return nil, err
+	}
+
 	in := &apiproto.BroadcastRequest{
 		Channels: channels,
-		Data:     apiproto.Raw(data),
+		Data:     apiproto.Raw(jsonData),
 	}
 	return client.Broadcast(ctx, in)
 }
