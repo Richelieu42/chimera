6faa372e272d96b9cbbe5973c8432ac5b1aa3672
Richelieu <richelieu042@gmail.com>
2024-05-27 11:08:25 +0800

    dev

--- src/component/web/request/httpClientKit/status_code.go
+++ src/component/web/request/httpClientKit/status_code.go
@@ -4,12 +4,12 @@ import (
 	"net/http"
 )
 
-// IsCodeValid 响应的http状态码 是否有效？
+// IsValidStatusCode 响应的http状态码 是否有效？
 /*
 PS:
 (1) 判断参考了jQuery;
 (2) 正常情况下，响应的http状态码有效的情况下，才会去读取响应的body.
 */
-func IsCodeValid(code int) bool {
+func IsValidStatusCode(code int) bool {
 	return code >= 200 && code < 300 || code == http.StatusNotModified
 }
