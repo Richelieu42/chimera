3b405f16c35599ee4949e393bd0c3d687cfba5c4
Richelieu <richelieu042@gmail.com>
2024-05-28 10:21:10 +0800

    dev

--- test/test.go
+++ test/test.go
@@ -1,23 +1,5 @@
 package main
 
-import (
-	"fmt"
-	"github.com/richelieu-yang/chimera/v3/src/core/errorKit"
-)
-
 func main() {
-	//if err := jsonKit.AssertJsonString("[{}", "jsonStr"); err != nil {
-	//	logrus.Error(err.Error())
-	//}
 
-	err := errorKit.Simplef("111")
-	fmt.Println(err.Error())
-
-	fmt.Println("===")
-
-	fmt.Printf("%v\n", err)
-
-	fmt.Println("===")
-
-	fmt.Printf("%+v\n", err)
 }
