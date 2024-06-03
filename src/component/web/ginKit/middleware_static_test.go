40f2e2e91436512036975b689322a31a99149e97
Richelieu <richelieu042@gmail.com>
2024-06-03 15:14:30 +0800

    dev

--- src/component/web/ginKit/middleware_static_test.go
+++ src/component/web/ginKit/middleware_static_test.go
@@ -1,29 +1,74 @@
 package ginKit
 
 import (
+	"embed"
 	"github.com/gin-gonic/gin"
 	"testing"
 )
 
 func TestNewStaticMiddleware(t *testing.T) {
 	engine := gin.Default()
-	engine.RedirectTrailingSlash = false
 
-	/*
-		e.g. 路由不包含 传参_test
-		http://127.0.0.1/a.txt
-	*/
-	m, err := NewStaticMiddleware("/", "_test", false)
-	if err != nil {
-		panic(err)
+	{
+		/*
+			e.g.
+			http://127.0.0.1/a.txt
+		*/
+		m, err := NewStaticMiddleware("/", "_test", false)
+		if err != nil {
+			panic(err)
+		}
+		engine.Use(m)
 	}
-	engine.Use(m)
+	{
+		/*
+			e.g.
+			http://127.0.0.1/c/a.txt
+		*/
+		m, err := NewStaticMiddleware("/c", "_test", false)
+		if err != nil {
+			panic(err)
+		}
+		engine.Use(m)
+	}
 
 	engine.GET("/ping", func(c *gin.Context) {
 		c.String(200, "OK")
 	})
 
-	// Listen and Server in 0.0.0.0:80
+	if err := engine.Run(":80"); err != nil {
+		panic(err)
+	}
+}
+
+//go:embed _test
+var staticFs embed.FS
+
+func TestNewStaticMiddlewareWithEmbedFolder(t *testing.T) {
+	engine := gin.Default()
+
+	{
+		/*
+			e.g.
+			http://127.0.0.1/a.txt
+		*/
+		m := NewStaticMiddlewareWithEmbedFolder("/", staticFs, "_test")
+		engine.Use(m)
+	}
+	{
+		// Richelieu: 此处有问题!!!
+		/*
+			e.g.
+			http://127.0.0.1/c/a.txt
+		*/
+		m := NewStaticMiddlewareWithEmbedFolder("/c", staticFs, "_test")
+		engine.Use(m)
+	}
+
+	engine.GET("/ping", func(c *gin.Context) {
+		c.String(200, "OK")
+	})
+
 	if err := engine.Run(":80"); err != nil {
 		panic(err)
 	}
--- test/test.go
+++ test/test.go
@@ -1,5 +1,27 @@
 package main
 
-func main() {
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/richelieu-yang/chimera/v3/src/component/web/ginKit"
+)
 
+func main() {
+	middleware, err := ginKit.NewStaticMiddleware("/", "_chimera-lib", true)
+	if err != nil {
+		panic(err)
+	}
+
+	engine := gin.Default()
+
+	engine.Use(middleware)
+
+	group := engine.Group("g")
+	//group.Use(middleware)
+	group.Any("/test", func(ctx *gin.Context) {
+		ctx.String(200, "ok")
+	})
+
+	if err := engine.Run(":80"); err != nil {
+		panic(err)
+	}
 }
