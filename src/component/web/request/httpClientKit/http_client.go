da4bbb77532a8807d459f19cbac00cae92d9801c
Richelieu <richelieu042@gmail.com>
2024-06-04 08:58:26 +0800

    dev

--- src/component/web/request/httpClientKit/http_client.go
+++ src/component/web/request/httpClientKit/http_client.go
@@ -9,6 +9,10 @@ import (
 // DefaultHttpClient 默认的 *http.Client 实例
 var DefaultHttpClient *http.Client = NewHttpClient(time.Second*3, true)
 
+// NewHttpClient
+/*
+@param insecureSkipVerify true: 跳过https证书验证（适用于: https证书是自己生成的，非法的）
+*/
 func NewHttpClient(timeout time.Duration, insecureSkipVerify bool) *http.Client {
 	return &http.Client{
 		Timeout: timeout,
