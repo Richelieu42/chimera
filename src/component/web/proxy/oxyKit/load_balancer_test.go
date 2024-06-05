33eaa97f8c6328257f5cdf2dbe7a88634438ea04
Richelieu <richelieu042@gmail.com>
2024-06-05 09:24:55 +0800

    dev

--- src/component/web/proxy/oxyKit/load_balancer_test.go
+++ src/component/web/proxy/oxyKit/load_balancer_test.go
@@ -1,29 +1,28 @@
 package oxyKit
 
 import (
+	_ "github.com/richelieu-yang/chimera/v3/src/log/logrusInitKit"
+
 	"github.com/gin-gonic/gin"
-	"github.com/richelieu-yang/chimera/v3/src/log/logrusInitKit"
 	"github.com/sirupsen/logrus"
 	"testing"
 )
 
 func TestNewLoadBalancerHandler(t *testing.T) {
-	logrusInitKit.SetUp()
-
 	logger := logrus.StandardLogger()
 	logger.SetLevel(logrus.DebugLevel)
 
 	lb, err := NewLoadBalancerHandler(nil, []string{
 		"http://127.0.0.1:8000",
-		"http://127.0.0.1:8001",
-		"http://127.0.0.1:8002",
+		//"http://127.0.0.1:8001",
+		//"http://127.0.0.1:8002",
 	}, logger, true)
 	if err != nil {
 		panic(err)
 	}
 
 	engine := gin.Default()
-	engine.Any("/test", func(ctx *gin.Context) {
+	engine.Any("/*path", func(ctx *gin.Context) {
 		lb(ctx.Writer, ctx.Request)
 	})
 	if err := engine.Run(":80"); err != nil {
