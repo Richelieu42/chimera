4cdc3c10bbe3d1c991c220ca96ae5b9997c77098
Richelieu <richelieu042@gmail.com>
2024-06-05 13:49:14 +0800

    dev

--- src/component/web/proxy/forwardKit/forward.go
+++ src/component/web/proxy/forwardKit/forward.go
@@ -7,7 +7,7 @@ import (
 	"net/http"
 )
 
-// ForwardToUrl 代理请求到指定url.
+// ForwardToUrl 代理请求到目标url.
 /*
 @param errorLog 可以为nil，即无输出
 @param url		目标url
@@ -27,7 +27,27 @@ func ForwardToUrl(w http.ResponseWriter, r *http.Request, errorLog *log.Logger, 
 	rp.ErrorHandler = func(w http.ResponseWriter, r *http.Request, err1 error) {
 		err = err1
 		if errorLog != nil {
-			errorLog.Printf("Fail to forward request to url(%s).", url)
+			errorLog.Printf("Fail to forward request to url(%s), error: %s.", url, err.Error())
+		}
+	}
+
+	err = rp.Forward(w, r)
+	return
+}
+
+func ForwardToHost(w http.ResponseWriter, r *http.Request, errorLog *log.Logger, host string, options ...DirectorOption) (err error) {
+	director, err := NewDirector(host, options...)
+	if err != nil {
+		return
+	}
+	rp, err := NewReverseProxy(director, nil, nil, errorLog, nil)
+	if err != nil {
+		return
+	}
+	rp.ErrorHandler = func(w http.ResponseWriter, r *http.Request, err1 error) {
+		err = err1
+		if errorLog != nil {
+			errorLog.Printf("Fail to forward request to host(%s), error: %s.", host, err.Error())
 		}
 	}
 
