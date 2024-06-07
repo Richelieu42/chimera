43faaa672c1c6000009fb6f5f68946a116fcede2
Richelieu <richelieu042@gmail.com>
2024-06-07 14:04:44 +0800

    dev

--- src/contextKit/key_value.go
+++ src/contextKit/key_value.go
@@ -3,6 +3,9 @@ package contextKit
 import "context"
 
 // WithKeyValue 在现有的上下文 (context) 中存储 1个 键值对，并返回一个新的上下文.
+/*
+@param ctx 不能为nil
+*/
 func WithKeyValue[K comparable, V any](ctx context.Context, k K, v V) context.Context {
 	return context.WithValue(ctx, k, v)
 }
