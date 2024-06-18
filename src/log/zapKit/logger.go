ccda1fab98b02d5fd184b72b55a8d68944b5c5cb
Richelieu <richelieu042@gmail.com>
2024-06-18 16:34:46 +0800

    dev

--- src/log/zapKit/logger.go
+++ src/log/zapKit/logger.go
@@ -7,18 +7,18 @@ import (
 
 // NewLogger
 /*
-@param options 	可以为nil，默认: 	(1) 输出到os.Stdout
-								(2) DEBUG级别
-								(3) 人类可读的多行输出
-								(4) 生产环境
+@param options 	可以为nil，默认: 	(1) 输出到os.Stdout;
+								(2) DEBUG级别;
+								(3) 人类可读的多行输出;
+								(4) 生产环境(development == false)
 */
 func NewLogger(options ...LoggerOption) (logger *zap.Logger) {
 	opts := loadOptions(options...)
 
+	/* encoder */
 	encoderConfig := zap.NewProductionEncoderConfig()
 	encoderConfig.EncodeTime = opts.EncodeTime
 	encoderConfig.EncodeLevel = opts.EncodeLevel
-
 	var encoder zapcore.Encoder
 	if opts.IsOutputTypeConsole() {
 		encoder = zapcore.NewConsoleEncoder(encoderConfig)
@@ -26,8 +26,10 @@ func NewLogger(options ...LoggerOption) (logger *zap.Logger) {
 		encoder = zapcore.NewJSONEncoder(encoderConfig)
 	}
 
+	/* core */
 	core := zapcore.NewCore(encoder, opts.WriteSyncer, opts.Level)
 
+	/* logger */
 	zapOptions := []zap.Option{zap.WithCaller(opts.Caller)}
 	if opts.Development {
 		zapOptions = append(zapOptions, zap.Development())
