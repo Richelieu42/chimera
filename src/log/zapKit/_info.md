ca59c86097edde06fdf7d896f9d15e0dc57a58fa
Richelieu <richelieu042@gmail.com>
2024-06-18 10:05:33 +0800

    dev

--- src/log/zapKit/_info.md
+++ src/log/zapKit/_info.md
@@ -1,7 +1,7 @@
 ## 参考
 
+- notes/Golang/log（日志）/zap.wps
 - [github 21.2k](https://github.com/uber-go/zap)
 - [Go 语言之 zap 日志库简单使用](https://zhuanlan.zhihu.com/p/637747131)
-    
-
+  
     
--- test/test0/test0.go
+++ test/test0/test0.go
@@ -1,9 +1,33 @@
 package main
 
 import (
-	_ "github.com/richelieu-yang/chimera/v3/src/log/logrusInitKit"
+	"go.uber.org/zap"
+	"time"
 )
 
 func main() {
+	url := "https:///www.baidu.com"
 
+	zap.New()
+
+	zap.Config{}
+	zap.SamplingConfig{}
+
+	zap.NewDevelopment()
+	zap.NewProduction()
+	zap.NewExample()
+	zap.NewNop()
+
+	zap.L()
+
+	logger, _ := zap.NewProduction()
+	defer logger.Sync() // flushes buffer, if any
+	sugar := logger.Sugar()
+	sugar.Infow("failed to fetch URL",
+		// Structured context as loosely typed key-value pairs.
+		"url", url,
+		"attempt", 3,
+		"backoff", time.Second,
+	)
+	sugar.Infof("Failed to fetch URL: %s", url)
 }
