c5640232129ce27777dcedb2107fb3b553badd9a
Richelieu <richelieu042@gmail.com>
2024-06-26 13:34:18 +0800

    dev

--- src/log/zapKit/global_test.go
+++ src/log/zapKit/global_test.go
@@ -6,8 +6,12 @@ import (
 )
 
 func TestDebug(t *testing.T) {
+	defer Sync()
+
+	/* Logger */
 	Debug("hello world", zap.String("key", "value"), zap.Bool("flag", true))
 
+	/* Sugar Logger */
 	Debugf("hello %s", "world")
 	Debugw("hello world", "key", "value", "flag", true)
 	Debugln("hello", "world")
