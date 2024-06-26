57fa137c6913951646a6bc5ac9878c498462006d
Richelieu <richelieu042@gmail.com>
2024-06-26 11:31:33 +0800

    dev

--- src/log/zapKit/global.go
+++ src/log/zapKit/global.go
@@ -3,6 +3,7 @@ package zapKit
 import "go.uber.org/zap"
 
 var (
+	l  *zap.Logger
 	sl *zap.SugaredLogger
 )
 
@@ -10,27 +11,52 @@ func init() {
 	/*
 		WithCallerSkip(1): 跳过1层，因为进行了1层封装
 	*/
-	sl = NewSugarLogger(WithCallerSkip(1))
+	l = NewLogger(WithCallerSkip(1))
+	sl = l.Sugar()
 }
 
 func Sync() error {
 	return sl.Sync()
 }
 
-func Info(args ...interface{}) {
-	sl.Info(args...)
+func Debug(msg string, fields ...zap.Field) {
+	l.Debug(msg, fields...)
 }
 
-// Infof 格式化输出的信息日志，类似于 fmt.Printf
-func Infof(template string, args ...interface{}) {
-	sl.Infof(template, args...)
+func Info(msg string, fields ...zap.Field) {
+	l.Info(msg, fields...)
 }
 
-// Infow 结构化输出的信息日志
-func Infow(msg string, keysAndValues ...interface{}) {
-	sl.Infow(msg, keysAndValues...)
+func Warn(msg string, fields ...zap.Field) {
+	l.Warn(msg, fields...)
 }
 
-func Infoln(args ...interface{}) {
-	sl.Infoln(args...)
+func Error(msg string, fields ...zap.Field) {
+	l.Error(msg, fields...)
 }
+
+func Panic(msg string, fields ...zap.Field) {
+	l.Panic(msg, fields...)
+}
+
+func DPanic(msg string, fields ...zap.Field) {
+	l.DPanic(msg, fields...)
+}
+
+func Fatal(msg string, fields ...zap.Field) {
+	l.Fatal(msg, fields...)
+}
+
+//// Infof 格式化输出的信息日志，类似于 fmt.Printf
+//func Infof(template string, args ...interface{}) {
+//	sl.Infof(template, args...)
+//}
+//
+//// Infow 结构化输出的信息日志
+//func Infow(msg string, keysAndValues ...interface{}) {
+//	sl.Infow(msg, keysAndValues...)
+//}
+//
+//func Infoln(args ...interface{}) {
+//	sl.Infoln(args...)
+//}
--- test/test.go
+++ test/test.go
@@ -8,15 +8,21 @@ import (
 func main() {
 	defer zapKit.Sync()
 
-	zapKit.Info("hello", "world")
+	//zapKit.Info("hello", "world")
 	zapKit.Info("hello", zap.String("key", "value"), zap.Bool("flag", true))
-	zapKit.Infof("hello %s", "world")
-	zapKit.Infow("hello")
-	zapKit.Infoln("hello", "world")
+	//zapKit.Infof("hello %s", "world")
+	//zapKit.Infow("hello")
+	//zapKit.Infoln("hello", "world")
+	//
+	//zapKit.Infow("This is an info message with structured data",
+	//	"key1", "value1",
+	//	"key2", 42,
+	//)
 
-	zapKit.Infow("This is an info message with structured data",
-		"key1", "value1",
-		"key2", 42,
-	)
+	logger := zapKit.NewLogger()
+	logger.Info("hello", zap.String("key", "value"), zap.Bool("flag", true))
 
+	logger.Info()
+
+	logger.Sugar().Info("hello", zap.String("key", "value"), zap.Bool("flag", true))
 }
