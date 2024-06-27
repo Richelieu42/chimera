b8b2f5e45d9d267d9fe5c04b1043bdb8bd694316
Richelieu <richelieu042@gmail.com>
2024-06-27 13:49:16 +0800

    dev

--- src/log/zapKit/nop.go
+++ src/log/zapKit/nop.go
@@ -1,1 +1,13 @@
 package zapKit
+
+import "go.uber.org/zap"
+
+// NewNopLogger 不进行实际日志记录操作.
+func NewNopLogger() *zap.Logger {
+	return zap.NewNop()
+}
+
+// NewNopSugaredLogger 不进行实际日志记录操作.
+func NewNopSugaredLogger() *zap.SugaredLogger {
+	return zap.NewNop().Sugar()
+}
