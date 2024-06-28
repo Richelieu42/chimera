b47b6316989ed7ad43d6b5d2ffca43f71a64fc2f
Richelieu <richelieu042@gmail.com>
2024-06-28 09:41:48 +0800

    dev

--- src/log/zapKit/core.go
+++ src/log/zapKit/core.go
@@ -8,7 +8,11 @@ import (
 // NewCore
 /*
 @param levelEnabler (1) zapcore.Level 类型实现了 zapcore.LevelEnabler 接口
-					(2)
+					(2) e.g. 自定义
+						// 创建错误日志级别的核心
+						errorLevel := zap.LevelEnablerFunc(func(level zapcore.Level) bool {
+							return level >= zapcore.ErrorLevel
+						})
 */
 func NewCore(enc zapcore.Encoder, ws zapcore.WriteSyncer, levelEnabler zapcore.LevelEnabler) zapcore.Core {
 	return zapcore.NewCore(enc, ws, levelEnabler)
