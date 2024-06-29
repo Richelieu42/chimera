8f29337a5a38e0e7528ed338320e2bb7f2baa9ff
Richelieu <richelieu042@gmail.com>
2024-06-29 15:44:08 +0800

    dev

--- src/log/zapKit/logger_test.go
+++ src/log/zapKit/logger_test.go
@@ -41,35 +41,38 @@ func TestNewLogger1(t *testing.T) {
 	l.Error("This is an error message0\nThis is an error message1")
 }
 
-//// TestNewLogger
-///*
-//第1个输出: DEBUG级别及以上的输出，输出到文件
-//第2个输出: WARN级别及以上的输出，输出到控制台
-//
-//总结: 	(1) < WARN级别，输出到: 文件
-//		(2) >= WARN级别，输出到: 文件、控制台
-//*/
-//func TestNewLogger(t *testing.T) {
-//	f, err := os.Create("_test.log")
-//	if err != nil {
-//		panic(err)
-//	}
-//	ws1 := zapcore.AddSync(f)
-//	ws2 := zapcore.AddSync(os.Stdout)
-//
-//	logger := NewLogger(
-//		WithOutputTypeConsole(),
-//		WithMessagePrefix("[TEST] "),
-//		WithInitialFields(zap.Bool("c", true)),
-//
-//		WithLevelEnabler(zapcore.DebugLevel),
-//		WithWriteSyncer(ws1),
-//
-//		WithOtherLevelEnabler(zapcore.WarnLevel),
-//		WithOtherWriteSyncer(ws2),
-//	)
-//	logger.Debug("This is a debug message", zap.String("key", "value"))
-//	logger.Info("This is an info message")
-//	logger.Warn("This is a warning message")
-//	logger.Error("This is an error message0\nThis is an error message1", zap.String("key", "value"), zap.Error(context.Canceled))
-//}
+/*
+level < WARN，输出到: 文件日志（JSON格式）
+level >= WARN，输出到: 文件日志（JSON格式） + 控制台（人类可读格式）
+*/
+func TestNewLogger2(t *testing.T) {
+	var core1, core2 zapcore.Core
+
+	/* 输出1: 文件 */
+	{
+		enc := NewEncoder(WithEncoderOutputFormatJson())
+
+		f, err := os.Create("_test.log")
+		if err != nil {
+			panic(err)
+		}
+		defer f.Close()
+		ws := zapcore.Lock(f)
+
+		core1 = NewCore(enc, ws, zapcore.DebugLevel, zap.String("source", "0"))
+	}
+	/* 输出2: 控制台 */
+	{
+
+		enc := NewEncoder(WithEncoderOutputFormatConsole())
+		ws := zapcore.Lock(os.Stdout)
+		core2 = NewCore(enc, ws, zapcore.WarnLevel, zap.String("source", "1"))
+	}
+
+	logger := NewLogger(MultiCore(core1, core2))
+	logger.Debug("This is a debug message", zap.String("key", "value"))
+	logger.Info("This is an info message")
+	logger.Warn("This is a warning message")
+	logger.Error("This is an error message0\nThis is an error message1", zap.String("key", "value"), zap.Error(context.Canceled))
+
+}
