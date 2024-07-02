d7c9f114da9a579549a76ca6ccfc357af5397156
Richelieu <richelieu042@gmail.com>
2024-07-02 09:19:26 +0800

    dev

--- test/test.go
+++ test/test.go
@@ -1,31 +1,22 @@
 package main
 
 import (
-	"fmt"
 	"github.com/richelieu-yang/chimera/v3/src/appKit"
-	"github.com/richelieu-yang/chimera/v3/src/core/signalKit"
+	"github.com/richelieu-yang/chimera/v3/src/log/zapKit"
 	"go.uber.org/zap"
 	"go.uber.org/zap/zapcore"
 )
 
-// 自定义的 Fatal Hook
-type myFatalHook struct{}
+type defaultFatalHook struct {
+}
 
-func (h *myFatalHook) OnWrite(*zapcore.CheckedEntry, []zap.Field) {
-	// 在这里实现你的钩子逻辑，比如执行清理工作
-	fmt.Println("Fatal hook triggered!")
+func (h *defaultFatalHook) OnWrite(entry *zapcore.CheckedEntry, fields []zapcore.Field) {
+	appKit.Exit(1)
 }
 
 func main() {
-	appKit.Exit()
+	l := zapKit.NewLogger(nil, zapKit.WithFatalHook(&defaultFatalHook{}))
+	//l := zapKit.NewLogger(nil)
 
-	signalKit.MonitorExitSignals()
-}
-
-func a() {
-	defer func() {
-		fmt.Println(666)
-	}()
-
-	panic("000")
+	l.Fatal("111", zap.String("key", "value"))
 }
