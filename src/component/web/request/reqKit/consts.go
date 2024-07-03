4cc332cf96fafd1622d371ce36e83bd4432eca94
Richelieu <richelieu042@gmail.com>
2024-07-03 11:10:37 +0800

    dev

--- src/component/web/request/reqKit/consts.go
+++ src/component/web/request/reqKit/consts.go
@@ -1,11 +1,11 @@
 package reqKit
 
-var (
+const (
 	ContentTypePlainText = "text/plain; charset=utf-8"
 
 	ContentTypeJson = "application/json; charset=utf-8"
 
 	ContentTypeXml = "text/xml; charset=utf-8"
 
-	ContentTypeForm = "application/x-www-form-urlencoded"
+	ContentTypeForm = "application/x-www-form-urlencoded; charset=utf-8"
 )
