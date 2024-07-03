631c2906b58c5c921ff211141c422db0491b0bbe
Richelieu <richelieu042@gmail.com>
2024-07-03 11:16:36 +0800

    dev

--- src/component/web/request/reqKit/method_post.go
+++ src/component/web/request/reqKit/method_post.go
@@ -5,14 +5,17 @@ import (
 	"github.com/imroc/req/v3"
 )
 
-func Post(ctx context.Context, url string, queryParams map[string][]string) *req.Response {
+// Post
+/*
+@param body Set the request Body, accepts string、[]byte、io.Reader、map and struct.
+*/
+func Post(ctx context.Context, url string, queryParams map[string][]string, body interface{}) *req.Response {
 	r := GetGlobalClient().Post(url)
 
 	for key, s := range queryParams {
 		r.AddQueryParams(key, s...)
 	}
-
-	r.SetContentType()
-
+	r.SetContentType(ContentTypeForm)
+	r.SetBody(body)
 	return r.Do(ctx)
 }
