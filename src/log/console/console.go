9006952ed2b2d04a4ef8e5fa57a99ab0e6e7b98c
Richelieu <richelieu042@gmail.com>
2024-07-04 15:53:11 +0800

    dev

--- src/log/console/console.go
+++ src/log/console/console.go
@@ -6,140 +6,63 @@ import (
 )
 
 var (
-	l *zap.Logger
-	s *zap.SugaredLogger
+	Sync func() = zapKit.Sync
+
+	PrintBasicDetails func(loggers ...*zap.SugaredLogger) = zapKit.PrintBasicDetails
+
+	Debug func(msg string, fields ...zap.Field) = zapKit.Debug
+
+	Info func(msg string, fields ...zap.Field) = zapKit.Info
+
+	Warn func(msg string, fields ...zap.Field) = zapKit.Warn
+
+	Error func(msg string, fields ...zap.Field) = zapKit.Error
+
+	Panic func(msg string, fields ...zap.Field) = zapKit.Panic
+
+	DPanic func(msg string, fields ...zap.Field) = zapKit.DPanic
+
+	Fatal func(msg string, fields ...zap.Field) = zapKit.Fatal
+
+	Debugf func(template string, args ...interface{}) = zapKit.Debugf
+
+	Debugw func(msg string, keysAndValues ...interface{}) = zapKit.Debugw
+
+	Debugln func(args ...interface{}) = zapKit.Debugln
+
+	Infof func(template string, args ...interface{}) = zapKit.Infof
+
+	Infow func(msg string, keysAndValues ...interface{}) = zapKit.Infow
+
+	Infoln func(args ...interface{}) = zapKit.Infoln
+
+	Warnf func(template string, args ...interface{}) = zapKit.Warnf
+
+	Warnw func(msg string, keysAndValues ...interface{}) = zapKit.Warnw
+
+	Warnln func(args ...interface{}) = zapKit.Warnln
+
+	Errorf func(template string, args ...interface{}) = zapKit.Errorf
+
+	Errorw func(msg string, keysAndValues ...interface{}) = zapKit.Errorw
+
+	Errorln func(args ...interface{}) = zapKit.Errorln
+
+	DPanicf func(template string, args ...interface{}) = zapKit.DPanicf
+
+	DPanicw func(msg string, keysAndValues ...interface{}) = zapKit.DPanicw
+
+	DPanicln func(args ...interface{}) = zapKit.DPanicln
+
+	Panicf func(template string, args ...interface{}) = zapKit.Panicf
+
+	Panicw func(msg string, keysAndValues ...interface{}) = zapKit.Panicw
+
+	Panicln func(args ...interface{}) = zapKit.Panicln
+
+	Fatalf func(template string, args ...interface{}) = zapKit.Fatalf
+
+	Fatalw func(msg string, keysAndValues ...interface{}) = zapKit.Fatalw
+
+	Fatalln func(args ...interface{}) = zapKit.Fatalln
 )
-
-func init() {
-	/*
-		WithCallerSkip(1): 跳过1层，因为进行了1层封装
-	*/
-	l = zapKit.NewLogger(nil, zapKit.WithCallerSkip(1))
-	s = l.Sugar()
-}
-
-func Sync() {
-	_ = l.Sync()
-	_ = s.Sync()
-}
-
-func Debug(msg string, fields ...zap.Field) {
-	l.Debug(msg, fields...)
-}
-
-func Info(msg string, fields ...zap.Field) {
-	l.Info(msg, fields...)
-}
-
-func Warn(msg string, fields ...zap.Field) {
-	l.Warn(msg, fields...)
-}
-
-func Error(msg string, fields ...zap.Field) {
-	l.Error(msg, fields...)
-}
-
-func Panic(msg string, fields ...zap.Field) {
-	l.Panic(msg, fields...)
-}
-
-func DPanic(msg string, fields ...zap.Field) {
-	l.DPanic(msg, fields...)
-}
-
-func Fatal(msg string, fields ...zap.Field) {
-	l.Fatal(msg, fields...)
-}
-
-// Debugf 格式化输出的信息日志，类似于fmt.Printf，可以使用格式化字符串.
-func Debugf(template string, args ...interface{}) {
-	s.Debugf(template, args...)
-}
-
-// Debugw 结构化输出的信息日志，使用键值对的方式输出，更加适合记录结构化数据.
-/*
-@param keysAndValues e.g. "key", "value", "flag", true
-*/
-func Debugw(msg string, keysAndValues ...interface{}) {
-	s.Debugw(msg, keysAndValues...)
-}
-
-// Debugln
-/*
-PS: Spaces are always added between arguments.（传参间会加上" "）
-*/
-func Debugln(args ...interface{}) {
-	s.Debugln(args...)
-}
-
-func Infof(template string, args ...interface{}) {
-	s.Infof(template, args...)
-}
-
-func Infow(msg string, keysAndValues ...interface{}) {
-	s.Infow(msg, keysAndValues...)
-}
-
-func Infoln(args ...interface{}) {
-	s.Infoln(args...)
-}
-
-func Warnf(template string, args ...interface{}) {
-	s.Warnf(template, args...)
-}
-
-func Warnw(msg string, keysAndValues ...interface{}) {
-	s.Warnw(msg, keysAndValues...)
-}
-
-func Warnln(args ...interface{}) {
-	s.Warnln(args...)
-}
-
-func Errorf(template string, args ...interface{}) {
-	s.Errorf(template, args...)
-}
-
-func Errorw(msg string, keysAndValues ...interface{}) {
-	s.Errorw(msg, keysAndValues...)
-}
-
-func Errorln(args ...interface{}) {
-	s.Errorln(args...)
-}
-
-func DPanicf(template string, args ...interface{}) {
-	s.DPanicf(template, args...)
-}
-
-func DPanicw(msg string, keysAndValues ...interface{}) {
-	s.DPanicw(msg, keysAndValues...)
-}
-
-func DPanicln(args ...interface{}) {
-	s.DPanicln(args...)
-}
-
-func Panicf(template string, args ...interface{}) {
-	s.Panicf(template, args...)
-}
-
-func Panicw(msg string, keysAndValues ...interface{}) {
-	s.Panicw(msg, keysAndValues...)
-}
-
-func Panicln(args ...interface{}) {
-	s.Panicln(args...)
-}
-
-func Fatalf(template string, args ...interface{}) {
-	s.Fatalf(template, args...)
-}
-
-func Fatalw(msg string, keysAndValues ...interface{}) {
-	s.Fatalw(msg, keysAndValues...)
-}
-
-func Fatalln(args ...interface{}) {
-	s.Fatalln(args...)
-}
