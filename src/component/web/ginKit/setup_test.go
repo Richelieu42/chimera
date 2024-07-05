e7a819ed31f0c9905a768eccf210a9a28ace40b6
Richelieu <richelieu042@gmail.com>
2024-07-05 13:30:34 +0800

    dev

--- src/component/web/ginKit/setup_test.go
+++ src/component/web/ginKit/setup_test.go
@@ -1,11 +1,11 @@
 package ginKit
 
 import (
-	"fmt"
 	"github.com/gin-gonic/gin"
 	"github.com/richelieu-yang/chimera/v3/src/config/viperKit"
 	"github.com/richelieu-yang/chimera/v3/src/consts"
 	"github.com/richelieu-yang/chimera/v3/src/core/pathKit"
+	"github.com/richelieu-yang/chimera/v3/src/log/console"
 	"testing"
 )
 
@@ -15,7 +15,7 @@ func TestMustSetUp(t *testing.T) {
 		if err != nil {
 			panic(err)
 		}
-		fmt.Println("wd:", wd)
+		console.Infof("working dir: %s", wd)
 	}
 
 	type config struct {
