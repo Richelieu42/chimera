11a2b333388b01056969900319cab26e3e3cec0c
Richelieu <richelieu042@gmail.com>
2024-07-06 14:13:53 +0800

    dev
     Conflicts:
    	src/log/console/logger.go

--- src/log/zapKit/print_sugar.go
+++ src/log/zapKit/print_sugar.go
@@ -1,46 +1,5 @@
 package zapKit
 
-import "go.uber.org/zap"
-
-var (
-	l = NewLogger(nil, WithCallerSkip(1))
-
-	s = l.Sugar()
-)
-
-func Sync() {
-	_ = l.Sync()
-	_ = s.Sync()
-}
-
-func Debug(msg string, fields ...zap.Field) {
-	l.Debug(msg, fields...)
-}
-
-func Info(msg string, fields ...zap.Field) {
-	l.Info(msg, fields...)
-}
-
-func Warn(msg string, fields ...zap.Field) {
-	l.Warn(msg, fields...)
-}
-
-func Error(msg string, fields ...zap.Field) {
-	l.Error(msg, fields...)
-}
-
-func Panic(msg string, fields ...zap.Field) {
-	l.Panic(msg, fields...)
-}
-
-func DPanic(msg string, fields ...zap.Field) {
-	l.DPanic(msg, fields...)
-}
-
-func Fatal(msg string, fields ...zap.Field) {
-	l.Fatal(msg, fields...)
-}
-
 // Debugf 格式化输出的信息日志，类似于fmt.Printf，可以使用格式化字符串.
 func Debugf(template string, args ...interface{}) {
 	s.Debugf(template, args...)
