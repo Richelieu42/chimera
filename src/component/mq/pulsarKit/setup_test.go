dc8a585132a775596da6add7ba9c82d773f98b6e
Richelieu <richelieu042@gmail.com>
2024-07-06 14:59:48 +0800

    init

--- src/component/mq/pulsarKit/setup_test.go
+++ src/component/mq/pulsarKit/setup_test.go
@@ -4,22 +4,24 @@ import (
 	"github.com/richelieu-yang/chimera/v3/src/config/viperKit"
 	"github.com/richelieu-yang/chimera/v3/src/consts"
 	"github.com/richelieu-yang/chimera/v3/src/core/pathKit"
-	"github.com/sirupsen/logrus"
+	"github.com/richelieu-yang/chimera/v3/src/log/console"
 	"testing"
 )
 
 func TestSetUp(t *testing.T) {
-	wd, err := pathKit.ReviseWorkingDirInTestMode(consts.ProjectName)
-	if err != nil {
-		panic(err)
+	{
+		wd, err := pathKit.ReviseWorkingDirInTestMode(consts.ProjectName)
+		if err != nil {
+			panic(err)
+		}
+		console.Infof("working dir: %s", wd)
 	}
-	logrus.Infof("wd: [%s].", wd)
-	path := "_chimera-lib/config.yaml"
 
 	type config struct {
 		Pulsar *Config `json:"pulsar"`
 	}
 
+	path := "_chimera-lib/config.yaml"
 	c := &config{}
 	if _, err := viperKit.UnmarshalFromFile(path, nil, c); err != nil {
 		panic(err)
@@ -28,82 +30,4 @@ func TestSetUp(t *testing.T) {
 		Topic: "test",
 		Print: true,
 	})
-
-	//topic := "test"
-	//consumer, err := NewConsumer(context.TODO(), pulsar.ConsumerOptions{
-	//	Topic:            topic,
-	//	SubscriptionName: idKit.NewULID(),
-	//	Type:             pulsar.Exclusive,
-	//}, "test-consumer.log")
-	//if err != nil {
-	//	logrus.Fatal(err)
-	//}
-	//defer consumer.Close()
-	//
-	//producer, err := NewProducer(context.TODO(), pulsar.ProducerOptions{
-	//	Topic:       topic,
-	//	SendTimeout: sendTimeout,
-	//}, "test-producer.log")
-	//if err != nil {
-	//	logrus.Fatal(err)
-	//}
-	//defer producer.Close()
-	//
-	///* 发消息 */
-	//go func() {
-	//	for i := 0; i < 3; i++ {
-	//		time.Sleep(time.Second)
-	//
-	//		text := strconv.Itoa(i)
-	//		id, err := producer.Send(context.TODO(), &pulsar.ProducerMessage{
-	//			Payload: []byte(text),
-	//		})
-	//		if err != nil {
-	//			logrus.WithFields(logrus.Fields{
-	//				"text": text,
-	//			}).Error("[PRODUCER] fail to send message")
-	//		} else {
-	//			logrus.WithFields(logrus.Fields{
-	//				"id":   id.String(),
-	//				"text": text,
-	//			}).Info("[PRODUCER] succeed to send message")
-	//		}
-	//	}
-	//}()
-	//
-	///* 收消息 */
-	//p, err := ants.NewPoolWithFunc(256, func(i interface{}) {
-	//	msg, ok := i.(pulsar.Message)
-	//	if !ok {
-	//		return
-	//	}
-	//	logrus.WithFields(logrus.Fields{
-	//		"id":   msg.ID().String(),
-	//		"text": string(msg.Payload()),
-	//	}).Info("[CONSUMER] receive a message")
-	//})
-	//if err != nil {
-	//	logrus.Fatal(err)
-	//}
-	//defer p.Release()
-	//for {
-	//	msg, err := consumer.Receive(context.TODO())
-	//	if err != nil {
-	//		logrus.Error("[CONSUMER] fail to receive")
-	//		break
-	//	}
-	//	if err := consumer.Ack(msg); err != nil {
-	//		logrus.WithFields(logrus.Fields{
-	//			"id":   msg.ID().String(),
-	//			"text": string(msg.Payload()),
-	//		}).Error("[CONSUMER] fail to ack")
-	//		continue
-	//	}
-	//
-	//	if err := p.Invoke(msg); err != nil {
-	//		logrus.WithFields(logrus.Fields{
-	//			"error": err.Error(),
-	//		}).Error("[CONSUMER] fail to invoke")
-	//	}
-	//}
 }
