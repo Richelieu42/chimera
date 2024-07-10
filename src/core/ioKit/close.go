9b5a6803af71172bebc882a96019c46260868103
Richelieu <richelieu042@gmail.com>
2024-07-10 13:16:43 +0800

    init

--- src/core/ioKit/close.go
+++ src/core/ioKit/close.go
@@ -8,7 +8,7 @@ import (
 // TryToClose 尝试关闭（如果实现了io.Closer接口的话）.
 /*
 @param writers 	(1) 可以为nil（即不传参）
-				(2) 其中可以有nil
+				(2) 其中元素可以为nil
 @return 发生error的话，返回第一个
 */
 func TryToClose(objs ...interface{}) (err error) {
@@ -27,7 +27,8 @@ func TryToClose(objs ...interface{}) (err error) {
 /*
 PS: 就算循环过程中返回了非nil的error，也要继续向下循环（尽可能多地关闭）.
 
-@param closers (1) 可以为nil（即不传参）；(2) 其中可以有nil
+@param closers 	(1) 可以为nil（即不传参）
+				(2) 其中元素可以为nil
 @return 发生error的话，返回第一个
 */
 func Close(closers ...io.Closer) (err error) {
