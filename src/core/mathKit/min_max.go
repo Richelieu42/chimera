dcd4195d2bb711db2740448d41aeb8d03f3a4d00
Richelieu <richelieu042@gmail.com>
2024-07-14 09:48:11 +0800

    init

--- src/core/mathKit/min_max.go
+++ src/core/mathKit/min_max.go
@@ -5,40 +5,18 @@ import (
 	"golang.org/x/exp/constraints"
 )
 
+func Min[T constraints.Integer | constraints.Float](numbers ...T) T {
+	return mathutil.Min(numbers...)
+}
+
 func MinBy[T any](slice []T, comparator func(T, T) bool) T {
 	return mathutil.MinBy(slice, comparator)
 }
 
+func Max[T constraints.Integer | constraints.Float](numbers ...T) T {
+	return mathutil.Max(numbers...)
+}
+
 func MaxBy[T any](slice []T, comparator func(T, T) bool) T {
 	return mathutil.MaxBy(slice, comparator)
-}
-
-// Max 获取最大值.
-/*
-PS: Go1.21即以上，建议使用内置函数 max().
-*/
-func Max[T constraints.Ordered](p T, args ...T) (max T) {
-	max = p
-
-	for _, ele := range args {
-		if ele > max {
-			max = ele
-		}
-	}
-	return
-}
-
-// Min 获取最小值.
-/*
-PS: Go1.21即以上，建议使用内置函数 min().
-*/
-func Min[T constraints.Ordered](p T, args ...T) (min T) {
-	min = p
-
-	for _, ele := range args {
-		if ele < min {
-			min = ele
-		}
-	}
-	return
 }
