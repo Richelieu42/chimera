87a6d022348a42c1a960d3e77e0a61d1287ae07c
Richelieu <richelieu042@gmail.com>
2024-07-18 15:55:00 +0800

    init

--- src/component/database/nosql/redis/redisKit/stream.go
+++ src/component/database/nosql/redis/redisKit/stream.go
@@ -36,24 +36,6 @@ func (client *Client) IsStreamSupported(ctx context.Context) error {
 	return nil
 }
 
-// XAdd [生产者] 添加消息到末尾（如果指定的队列不存在，则创建一个队列）.
-/*
-语法: XADD key ID field value [field value ...]
-key:			队列名称，如果不存在就创建
-ID:				消息 id，我们使用 * 表示由 redis 生成，可以自定义，但是要自己保证递增性。
-field value:	记录
-
-@param a 	(1) 必需字段: Stream、Values
-			(2) Stream字段对应: Redis中的key（stream类型）
-			(3) 可选的ID字段，为 ""（默认） 则由Redis生成
-@return 	id: 消息的id
-*/
-func (client *Client) XAdd(ctx context.Context, a *redis.XAddArgs) (id string, err error) {
-	cmd := client.universalClient.XAdd(ctx, a)
-	id, err = cmd.Result()
-	return
-}
-
 // XDel 删除Stream中的特定消息.
 /*
 @return (1) 删除成功: 返回(1, nil)
@@ -71,28 +53,5 @@ func (client *Client) XRead(ctx context.Context, a *redis.XReadArgs) ([]redis.XS
 
 func (client *Client) XReadStreams(ctx context.Context, streams ...string) ([]redis.XStream, error) {
 	cmd := client.universalClient.XReadStreams(ctx, streams...)
-	return cmd.Result()
-}
-
-// XReadGroup [消费者] 读取消费者组中的消息.("xreadgroup", "group")
-/*
-XReadGroupArgs结构体:
-	Group 		消费组名
-	Consumer	消费者名
-	Count		读取数量
-	Block		阻塞时间
-	Streams		要读取的所有Stream（!!!: (1) 数量应当>=2; (2) 最后一个元素应该是 ">"）
-*/
-func (client *Client) XReadGroup(ctx context.Context, a *redis.XReadGroupArgs) ([]redis.XStream, error) {
-	cmd := client.universalClient.XReadGroup(ctx, a)
-	return cmd.Result()
-}
-
-// XAck [消费者] 将消息标记为"已处理".
-/*
-PS: 并不会删除对应消息.
-*/
-func (client *Client) XAck(ctx context.Context, stream, group string, ids ...string) (int64, error) {
-	cmd := client.universalClient.XAck(ctx, stream, group, ids...)
 	return cmd.Result()
 }
---
+++ src/component/database/nosql/redis/redisKit/stream_consumer.go
@@ -0,0 +1,29 @@
+package redisKit
+
+import (
+	"context"
+	"github.com/redis/go-redis/v9"
+)
+
+// XReadGroup [消费者] 读取消费者组中的消息.("xreadgroup", "group")
+/*
+XReadGroupArgs结构体:
+	Group 		消费组名
+	Consumer	消费者名
+	Count		读取数量
+	Block		阻塞时间
+	Streams		要读取的所有Stream（!!!: (1) 数量应当>=2; (2) 最后一个元素应该是 ">"）
+*/
+func (client *Client) XReadGroup(ctx context.Context, a *redis.XReadGroupArgs) ([]redis.XStream, error) {
+	cmd := client.universalClient.XReadGroup(ctx, a)
+	return cmd.Result()
+}
+
+// XAck [消费者] 将消息标记为"已处理".
+/*
+PS: 并不会删除对应消息.
+*/
+func (client *Client) XAck(ctx context.Context, stream, group string, ids ...string) (int64, error) {
+	cmd := client.universalClient.XAck(ctx, stream, group, ids...)
+	return cmd.Result()
+}
