9f5b16d500d9656842fc4d69fb5cfdaa01ba2098
Richelieu <richelieu042@gmail.com>
2024-07-18 15:42:59 +0800

    init

--- src/component/database/nosql/redis/redisKit/setup_test.go
+++ src/component/database/nosql/redis/redisKit/setup_test.go
@@ -6,6 +6,7 @@ import (
 	"github.com/richelieu-yang/chimera/v3/src/config/viperKit"
 	"github.com/richelieu-yang/chimera/v3/src/consts"
 	"github.com/richelieu-yang/chimera/v3/src/core/pathKit"
+	"github.com/richelieu-yang/chimera/v3/src/log/console"
 	"github.com/richelieu-yang/chimera/v3/src/serialize/json/jsonKit"
 	"testing"
 )
@@ -37,29 +38,18 @@ func TestSetUp(t *testing.T) {
 	client = client
 
 	{
-		fmt.Println("---")
-		fmt.Println(client.Set(context.TODO(), "c", "zzz", 0))
-		fmt.Println(client.Get(context.TODO(), "c"))
-		fmt.Println("---")
+		err := client.XGroupCreate(context.TODO(), "stream:test", "gg", "$")
+		if err != nil {
+			console.Error(err.Error())
+		} else {
+			console.Info("OK")
+		}
 
-		//fmt.Println(client.Ping(context.TODO()))
-
-		//fmt.Println(client.IsStreamSupported(context.Background()))
-
-		//m := map[string]interface{}{}
-		//m["use"] = "pulsar"
-		//m["pulsar"] = &pulsarKit.Config{
-		//	Addrs: []string{"localhost:6650"},
-		//}
-		//
-		//jsonStr, err := jsonKit.MarshalToString(m)
-		//if err != nil {
-		//	panic(err)
-		//}
-		//count, err := client.Publish(context.TODO(), "WsMq", jsonStr)
-		//if err != nil {
-		//	panic(err)
-		//}
-		//fmt.Println(count)
+		err = client.XGroupCreate(context.TODO(), "stream:test", "gg", "$")
+		if err != nil {
+			console.Error(err.Error())
+		} else {
+			console.Info("OK")
+		}
 	}
 }
--- src/component/database/nosql/redis/redisKit/stream_create.go
+++ src/component/database/nosql/redis/redisKit/stream_create.go
@@ -10,12 +10,10 @@ import (
 /*
 命令: ("xgroup", "create", stream, group, start)
 
-@return 返回的error非nil的话，建议先进行 IsConsumerGroupNameAlreadyExistError 判断，返回true的话忽略该err.
-
-PS:
-(1) 如果 stream 对应的key:	(a) 存在，do nothing;
-							(b) 不存在，将返回error（ERR The XGROUP subcommand requires the key to exist. Note that for CREATE you may want to use the MKSTREAM option to create an empty stream automatically.）.
-(2) 如果 group 已经存在，将返回error(BUSYGROUP Consumer Group name already exists).
+@return (1) 如果 stream 不存在的话，会返回error:
+				ERR The XGROUP subcommand requires the key to exist. Note that for CREATE you may want to use the MKSTREAM option to create an empty stream automatically.
+		(2) stream 存在的情况下，如果 group 已经存在的话，会返回error（此种error建议忽略; 可以通过 IsConsumerGroupNameAlreadyExistError 判断）:
+				BUSYGROUP Consumer Group name already exists
 */
 func (client *Client) XGroupCreate(ctx context.Context, stream, group, start string) error {
 	resp, err := client.universalClient.XGroupCreate(ctx, stream, group, start).Result()
@@ -30,17 +28,14 @@ func (client *Client) XGroupCreate(ctx context.Context, stream, group, start str
 
 // XGroupCreateMkStream 创建消费者组Consumer Group（如果stream不存在，那么会自动创建一个空（长度为0）的stream）.
 /*
-命令: ("xgroup", "create", stream, group, start, "mkstream")
-
-@return 返回的error非nil的话，建议先进行 IsConsumerGroupNameAlreadyExistError 判断，返回true的话忽略该err.
-
 PS:
-(1) 如果 stream 对应的key:	(a) 存在，do nothing;
-							(b) 不存在，将自动创建一个空的stream.
-(2) 如果 group 已经存在，将返回error(BUSYGROUP Consumer Group name already exists).
-(3) MKSTREAM是一个可选子命令，如果指定了它，那么在创建消费者组的时候，如果stream不存在，那么会自动创建一个空（长度为0）的stream.
+(1) 命令: ("xgroup", "create", stream, group, start, "mkstream")
+	MKSTREAM是一个可选子命令，如果指定了它，那么在创建消费者组的时候，如果stream不存在，那么会自动创建一个空（长度为0）的stream
 
-@param stream 	要创建的 消费者组的流 的名称
+@return stream 存在的情况下，如果 group 已经存在的话，会返回error（此种error建议忽略; 可以通过 IsConsumerGroupNameAlreadyExistError 判断）:
+				BUSYGROUP Consumer Group name already exists
+
+@param stream 	要创建的 消费者组的流 的名称（不存在的话，会自动创建一个空的（长度为0）的stream）
 @param group 	要创建的 消费者组 的名称
 @param start 	起始 ID，可以是具体的 ID 或特殊的 "$"
 				e.g.
