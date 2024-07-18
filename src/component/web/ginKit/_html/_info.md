e7ce495e0be54077351ac88cf42e2e587a1eb986
Richelieu <richelieu042@gmail.com>
2024-07-18 14:08:43 +0800

    init

--- src/component/web/ginKit/_html/_info.md
+++ src/component/web/ginKit/_html/_info.md
@@ -1,2 +1,7 @@
+## 全局安装 html-minifier（以便后续使用 html-minifier 命令）
+
+npm install -g html-minifier
+
 ## 命令（压缩404.html）
+
 html-minifier --collapse-whitespace --remove-comments --remove-optional-tags --remove-redundant-attributes --remove-script-type-attributes --remove-tag-whitespace --use-short-doctype --minify-css true --minify-js true -o 404.min.html 404.html
--- src/component/web/ginKit/no_route.go
+++ src/component/web/ginKit/no_route.go
@@ -1,9 +1,7 @@
 package ginKit
 
 import (
-	"fmt"
 	"github.com/gin-gonic/gin"
-	"github.com/richelieu-yang/chimera/v3/src/core/strKit"
 	"html/template"
 	"net/http"
 )
@@ -17,15 +15,15 @@ func DefaultNoRouteHtml(engine *gin.Engine) error {
 	}
 	engine.SetHTMLTemplate(templ)
 
-	var prefix string
-	if strKit.IsNotEmpty(serviceInfo) {
-		prefix = fmt.Sprintf("[%s] ", serviceInfo)
-	}
+	//var prefix string
+	//if strKit.IsNotEmpty(serviceInfo) {
+	//	prefix = fmt.Sprintf("[%s] ", serviceInfo)
+	//}
 
 	engine.NoRoute(func(ctx *gin.Context) {
 		ctx.HTML(http.StatusNotFound, "404.min.html", gin.H{
-			"prefix": prefix,
-			"route":  ctx.Request.URL.Path,
+			//"prefix": prefix,
+			"route": ctx.Request.URL.Path,
 		})
 	})
 	return nil
