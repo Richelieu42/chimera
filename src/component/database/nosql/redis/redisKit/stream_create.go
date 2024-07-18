c13dab60217de8cb8dd502e948035b406151e5fb
Richelieu <richelieu042@gmail.com>
2024-07-18 15:24:17 +0800

    init

--- src/component/database/nosql/redis/redisKit/stream_create.go
+++ src/component/database/nosql/redis/redisKit/stream_create.go
@@ -6,11 +6,11 @@ import (
 	"github.com/richelieu-yang/chimera/v3/src/core/strKit"
 )
 
-// XGroupCreate 创建消费者组（Consumer Group）.
+// XGroupCreate 创建消费者组Consumer Group.
 /*
-("xgroup", "create", stream, group, start)
+命令: ("xgroup", "create", stream, group, start)
 
-!!!: 返回的err非nil的话，建议先进行 IsConsumerGroupNameAlreadyExistError 判断，返回true的话忽略该err.
+@return 返回的error非nil的话，建议先进行 IsConsumerGroupNameAlreadyExistError 判断，返回true的话忽略该err.
 
 PS:
 (1) 如果 stream 对应的key:	(a) 存在，do nothing;
@@ -28,11 +28,11 @@ func (client *Client) XGroupCreate(ctx context.Context, stream, group, start str
 	return nil
 }
 
-// XGroupCreateMkStream 创建消费者组（Consumer Group），如果stream不存在，那么会自动创建一个空（长度为0）的stream.
+// XGroupCreateMkStream 创建消费者组Consumer Group（如果stream不存在，那么会自动创建一个空（长度为0）的stream）.
 /*
-("xgroup", "create", stream, group, start, "mkstream")
+命令: ("xgroup", "create", stream, group, start, "mkstream")
 
-!!!: 返回的err非nil的话，建议先进行 IsConsumerGroupNameAlreadyExistError 判断，返回true的话忽略该error.
+@return 返回的error非nil的话，建议先进行 IsConsumerGroupNameAlreadyExistError 判断，返回true的话忽略该err.
 
 PS:
 (1) 如果 stream 对应的key:	(a) 存在，do nothing;
