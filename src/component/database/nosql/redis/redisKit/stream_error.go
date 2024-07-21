62d90b1a178e4219ef08e48996883367075049d7
Richelieu <richelieu042@gmail.com>
2024-07-21 15:49:30 +0800

    init

--- src/component/database/nosql/redis/redisKit/stream_error.go
+++ src/component/database/nosql/redis/redisKit/stream_error.go
@@ -5,11 +5,11 @@ import (
 	"github.com/richelieu-yang/chimera/v3/src/core/strKit"
 )
 
-// IsConsumerGroupNameAlreadyExistError 适用场景:
+// IsConsumerGroupNameAlreadyExistError 适用场景: 创建Consumer group（XGroupCreate || XGroupCreateMkStream）时，返回error.
 /*
 TODO: 涉及 github.com/redis/go-redis/v9 源码， 后续看有没有好的解决方法.
 
-PS: 与 XGroupCreateMkStream 搭配使用.
+@return true: 错误是由 "指定group已经存在（无需重复创建）" 导致的.
 */
 func IsConsumerGroupNameAlreadyExistError(err error) bool {
 	if err == nil {
@@ -20,8 +20,10 @@ func IsConsumerGroupNameAlreadyExistError(err error) bool {
 	return strKit.ContainsIgnoreCase(err.Error(), "BUSYGROUP Consumer Group name already exists")
 }
 
-// IsNoStreamOrNoGroupError 适用场景: Consumer读取消息时返回error.
+// IsNoStreamOrNoGroupError 适用场景: Consumer读取消息时，返回error.
 /*
+TODO: 涉及 github.com/redis/go-redis/v9 源码， 后续看有没有好的解决方法.
+
 @return true: 错误是由 "指定stream不存在" 或 "指定stream存在，但指定group不存在" 导致的.
 */
 func IsNoStreamOrNoGroupError(err error) bool {
