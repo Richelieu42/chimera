31637137a6d5532812fcc60a60aef44e9e9cc973
Richelieu <richelieu042@gmail.com>
2024-07-24 13:47:09 +0800

    init

--- src/contextKit/key_value.go
+++ src/contextKit/key_value.go
@@ -2,7 +2,7 @@ package contextKit
 
 import "context"
 
-// WithKeyValue 在现有的上下文 (context) 中存储 1个 键值对，并返回一个新的上下文.
+// AttachKeyValue 在现有的上下文 (context) 中存储 1个 键值对，并返回一个新的上下文.
 /*
 PS:
 (1) 同一个Context实例，多次调用此方法 且 key相同，则覆盖之前的值.
@@ -10,18 +10,18 @@ PS:
 
 @param ctx 不能为nil
 */
-func WithKeyValue[K comparable, V any](ctx context.Context, k K, v V) context.Context {
+func AttachKeyValue[K comparable, V any](ctx context.Context, k K, v V) context.Context {
 	return context.WithValue(ctx, k, v)
 }
 
-// WithKeyValueMap 在现有的上下文 (context) 中存储 多个 键值对，并返回一个新的上下文.
+// AttachKeyValueMap 在现有的上下文 (context) 中存储 多个 键值对，并返回一个新的上下文.
 /*
 Deprecated: 如果传参m的元素数量多，可能会导致嵌套太多层，这样不太好.
 			如果 键值对多 || 会频繁修改键值对，建议使用 gorilla/context（可能导致内存泄露!!!）.
 
 @param ctx 不能为nil
 */
-func WithKeyValueMap[K comparable, V any](ctx context.Context, m map[K]V) context.Context {
+func AttachKeyValueMap[K comparable, V any](ctx context.Context, m map[K]V) context.Context {
 	for k, v := range m {
 		ctx = context.WithValue(ctx, k, v)
 	}
--- src/contextKit/key_value_test.go
+++ src/contextKit/key_value_test.go
@@ -3,20 +3,61 @@ package contextKit
 import (
 	"context"
 	"fmt"
+	"github.com/gin-gonic/gin"
+	"github.com/richelieu-yang/chimera/v3/src/component/web/proxy/forwardKit"
+	"github.com/richelieu-yang/chimera/v3/src/log/console"
+	"github.com/richelieu-yang/chimera/v3/src/netKit"
+	"go.uber.org/zap"
 	"testing"
+	"time"
 )
 
-func TestWithKeyValue(t *testing.T) {
+func TestAttachKeyValue(t *testing.T) {
 	ctx := context.Background()
 
-	ctx = WithKeyValue(ctx, "key", "a")
+	ctx = AttachKeyValue(ctx, "key", "a")
 	fmt.Println("value:", ctx.Value("key")) // value: a
 
-	ctx = WithKeyValue(ctx, "key", "b")
-	ctx = WithKeyValue(ctx, "key", "c")
+	ctx = AttachKeyValue(ctx, "key", "b")
+	ctx = AttachKeyValue(ctx, "key", "c")
 	fmt.Println("value:", ctx.Value("key")) // value: c
 }
 
-func TestWithKeyValue1(t *testing.T) {
+// 启动后，需要手动访问url: http://127.0.0.1:8001/test
+func TestAttachKeyValue1(t *testing.T) {
+	go func() {
+		port := 8002
+		engine := gin.Default()
+		engine.Any("/test", func(ctx *gin.Context) {
+			/* 拿不到 */
+			console.Info("[8002]", zap.Any("value", ctx.Request.Context().Value("key")))
 
+			ctx.String(200, fmt.Sprintf("[%d] Hello world!", port))
+		})
+		if err := engine.Run(netKit.JoinToHost("", port)); err != nil {
+			panic(err)
+		}
+	}()
+
+	time.Sleep(time.Second)
+
+	port := 8001
+	engine := gin.Default()
+	engine.Any("/test", func(ctx *gin.Context) {
+		tmpCtx := AttachKeyValue(ctx.Request.Context(), "key", "a")
+		ctx.Request = ctx.Request.WithContext(tmpCtx)
+		/* 能拿到 */
+		console.Info("[8001]", zap.Any("value", ctx.Request.Context().Value("key")))
+
+		if err := forwardKit.ForwardToHostComplexly(ctx.Writer, ctx.Request, "127.0.0.1:8002", nil, nil, nil); err != nil {
+			console.Error("Fail to forward.", zap.String("error", err.Error()))
+			ctx.String(502, err.Error())
+			return
+		}
+		/* 能拿到 */
+		console.Info("[8001]", zap.Any("value", ctx.Request.Context().Value("key")))
+	})
+	if err := engine.Run(netKit.JoinToHost("", port)); err != nil {
+		panic(err)
+	}
 }
