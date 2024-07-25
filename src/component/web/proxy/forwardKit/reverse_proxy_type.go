11922349a9ef6820623e5c31a604ee7b6114661f
Richelieu <richelieu042@gmail.com>
2024-07-25 09:14:19 +0800

    init

--- src/component/web/proxy/forwardKit/reverse_proxy_type.go
+++ src/component/web/proxy/forwardKit/reverse_proxy_type.go
@@ -8,6 +8,7 @@ import (
 	"net/http/httputil"
 )
 
+// ReverseProxy 不同于 httputil.ReverseProxy，此结构体的 Forward 方法如果代理失败会返回error.
 type ReverseProxy struct {
 	// !!!: 此处不能是 *httputil.ReverseProxy，因为会在 Forward 方法体内修改receiver的字段，但不希望修改方法体外的.
 	httputil.ReverseProxy
