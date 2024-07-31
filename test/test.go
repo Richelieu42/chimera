7135ef3a5708c31d53f36bac3710d5d1ee07dbf7
Richelieu <richelieu042@gmail.com>
2024-07-31 10:26:52 +0800

    init

--- test/test.go
+++ test/test.go
@@ -1,12 +1,20 @@
 package main
 
 import (
+	"github.com/richelieu-yang/chimera/v3/src/core/osKit"
 	"github.com/richelieu-yang/chimera/v3/src/log/console"
 	"github.com/richelieu-yang/chimera/v3/src/serialize/json/jsonKit"
 )
 
 func main() {
-	json, err := jsonKit.MarshalToString(1)
+	console.Infof("os: %s", osKit.OS)
+	console.Infof("arch: %s", osKit.ARCH)
+	console.Infof("json library: %s", jsonKit.GetLibrary())
+
+	json, err := jsonKit.MarshalToString(map[string]interface{}{
+		"a": true,
+		"0": []interface{}{true, false, 0, "ccc"},
+	})
 	if err != nil {
 		panic(err)
 	}
