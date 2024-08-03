3059fbb25d5882a2234b5e7f8e44891113144c7d
Richelieu <richelieu042@gmail.com>
2024-08-03 14:29:12 +0800

    init

--- src/log/console/console_format.go
+++ src/log/console/console_format.go
@@ -6,31 +6,38 @@ var (
 	innerS = zapKit.GetInnerSugaredLogger()
 )
 
+// Debugf TODO: 暂不将 zapKit.Debugf 赋值给函数变量，原因: GoLand不支持.
 func Debugf(template string, args ...interface{}) {
+	zapKit.Debugf(template, args...)
 	innerS.Debugf(template, args...)
 }
 
-// Infof 格式化输出的信息日志，类似于 fmt.Printf ，可以使用格式化字符串.
+// Infof TODO: 暂不将 zapKit.Debugf 赋值给函数变量，原因: GoLand不支持.
 func Infof(template string, args ...interface{}) {
 	innerS.Infof(template, args...)
 }
 
+// Warnf TODO: 暂不将 zapKit.Debugf 赋值给函数变量，原因: GoLand不支持.
 func Warnf(template string, args ...interface{}) {
 	innerS.Warnf(template, args...)
 }
 
+// Errorf TODO: 暂不将 zapKit.Debugf 赋值给函数变量，原因: GoLand不支持.
 func Errorf(template string, args ...interface{}) {
 	innerS.Errorf(template, args...)
 }
 
+// DPanicf TODO: 暂不将 zapKit.Debugf 赋值给函数变量，原因: GoLand不支持.
 func DPanicf(template string, args ...interface{}) {
 	innerS.DPanicf(template, args...)
 }
 
+// Panicf TODO: 暂不将 zapKit.Debugf 赋值给函数变量，原因: GoLand不支持.
 func Panicf(template string, args ...interface{}) {
 	innerS.Panicf(template, args...)
 }
 
+// Fatalf TODO: 暂不将 zapKit.Debugf 赋值给函数变量，原因: GoLand不支持.
 func Fatalf(template string, args ...interface{}) {
 	innerS.Fatalf(template, args...)
 }
