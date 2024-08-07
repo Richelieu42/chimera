014e3c22606901e14596f4a63ebb6d690df4abe6
Richelieu <richelieu042@gmail.com>
2024-08-07 13:57:29 +0800

    init

--- test/test.go
+++ test/test.go
@@ -1,13 +1,5 @@
 package main
 
-import (
-	"fmt"
-)
-
 func main() {
-	//str := "你好，世界"
-	//byteLength := len(str)
-	//fmt.Printf("字符串 '%s' 的 UTF-8 字节长度是 %d\n", str, byteLength)
 
-	fmt.Println(len("亲爱的宝宝，你是妈妈和爸爸最珍贵的宝贝。这个世界充满了美丽和惊喜，等待着你来探索。每天清晨，阳光洒在你的脸上，花儿在微风中摇曳，小鸟在树梢上歌唱。我们希望你在未来的日子里，能够感受到这些美好的事物，享受每一个快乐的瞬间，健康快乐地成长。"))
 }
