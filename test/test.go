26cf32bd16ef48d120254533ea0fbadb41d0cff7
Richelieu <richelieu042@gmail.com>
2024-08-13 09:41:00 +0800

    init

--- test/test.go
+++ test/test.go
@@ -1,5 +1,14 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"strings"
+)
 
+func main() {
+	sb := &strings.Builder{}
+
+	sb.WriteString("hello\n")
+	sb.WriteString("world\n")
+	fmt.Println(sb.String())
 }
