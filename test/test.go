211ea77a6b44fd000743d9dfb99143fb6f06c2d7
Richelieu <richelieu042@gmail.com>
2024-08-14 14:24:54 +0800

    init

--- test/test.go
+++ test/test.go
@@ -1,16 +1,5 @@
 package main
 
-import (
-	"fmt"
-	"github.com/samber/lo"
-	"math"
-)
-
 func main() {
-	fmt.Println(lo.Elipse("Richelieu", 0))
-	fmt.Println(lo.Elipse("Richelieu", 1))
-	fmt.Println(lo.Elipse("Richelieu", 2))
-	fmt.Println(lo.Elipse("Richelieu", 3))
-	fmt.Println(lo.Elipse("Richelieu", 4))
-	fmt.Println(lo.Elipse("Richelieu", math.MaxInt))
+
 }
