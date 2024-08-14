61e22d1b55463bee6a05a5868063940bae3eab74
Richelieu <richelieu042@gmail.com>
2024-08-14 13:25:18 +0800

    init

--- src/core/strKit/intercept.go
+++ src/core/strKit/intercept.go
@@ -1,5 +1,30 @@
 package strKit
 
+import "github.com/samber/lo"
+
+// Substring Return part of a string.
+func Substring[T ~string](str T, offset int, length uint) T {
+	return lo.Substring(str, offset, length)
+}
+
+//// Substring 类似：Java的String.substring()
+///*
+//@param from	开始的下标（包括）
+//@param to	结束的下标（不包括）
+//@return 范围: [from, to)
+//
+//要点:
+//(1) from和to都不能 < 0
+//(2) 必须满足条件: from <= to（如果from == to，将返回""）
+//(3) 下标不能越界
+//
+//e.g.
+//("abcd", 1, 1) => ""
+//*/
+//func Substring(str string, from, to int) string {
+//	return str[from:to]
+//}
+
 // SubBefore
 /*
 @param index 不包括
@@ -50,22 +75,4 @@ func SubAfterString(s, str string) string {
 		return SubAfter(s, i)
 	}
 	return s
-}
-
-// Substring 类似：Java的String.substring()
-/*
-@param from	开始的下标（包括）
-@param to	结束的下标（不包括）
-@return 范围: [from, to)
-
-要点:
-(1) from和to都不能 < 0
-(2) 必须满足条件: from <= to（如果from == to，将返回""）
-(3) 下标不能越界
-
-e.g.
-("abcd", 1, 1) => ""
-*/
-func Substring(str string, from, to int) string {
-	return str[from:to]
 }
