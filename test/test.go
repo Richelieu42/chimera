52ed16f2d66d239a5f4df7f68de48c2ec0d5b234
Richelieu <richelieu042@gmail.com>
2024-08-15 13:46:11 +0800

    init

--- test/test.go
+++ test/test.go
@@ -1,5 +1,18 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"github.com/gin-gonic/gin"
+	"github.com/richelieu-yang/chimera/v3/src/netKit"
+)
 
+func main() {
+	port := 8000
+	engine := gin.Default()
+	engine.Any("/test", func(ctx *gin.Context) {
+		ctx.String(200, fmt.Sprintf("[%d] Hello world!", port))
+	})
+	if err := engine.Run(netKit.JoinToHost("", port)); err != nil {
+		panic(err)
+	}
 }
