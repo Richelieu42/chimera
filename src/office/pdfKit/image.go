d6a8705efe06a399a23247f6fb2e728898b80671
Richelieu <richelieu042@gmail.com>
2024-08-19 16:30:53 +0800

    init

---
+++ src/office/pdfKit/image.go
@@ -0,0 +1,25 @@
+package pdfKit
+
+import (
+	"github.com/pdfcpu/pdfcpu/pkg/api"
+	"github.com/pdfcpu/pdfcpu/pkg/pdfcpu/model"
+	"github.com/richelieu-yang/chimera/v3/src/file/fileKit"
+)
+
+// ExtractImagesFile 导出pdf文件中的图片.
+/*
+!!!: pdfcpu 库仅支持提取原始嵌入的图像。如果图像经过压缩或编码，则可能无法提取。
+
+@param outDir			输出目录
+@param selectedPages	可以为nil，即该pdf文件的所有页
+						e.g. 第1页和第2页
+							[]string{"1", "2"}
+@param conf				可以为nil
+*/
+func ExtractImagesFile(inFile, outDir string, selectedPages []string, conf *model.Configuration) error {
+	if err := fileKit.MkDirs(outDir); err != nil {
+		return err
+	}
+
+	return api.ExtractImagesFile(inFile, outDir, selectedPages, conf)
+}
--- test/test.go
+++ test/test.go
@@ -1,5 +1,35 @@
 package main
 
+import (
+	"fmt"
+	"github.com/pdfcpu/pdfcpu/pkg/api"
+	"log"
+	"os"
+)
+
 func main() {
-	api.Spli
+	// 定义输入 PDF 文件和输出目录
+	inputPDF := "/Users/richelieu/Desktop/34页(1).pdf"
+	outputDir := "_images"
+
+	// 创建输出目录
+	err := os.MkdirAll(outputDir, os.ModePerm)
+	if err != nil {
+		log.Fatalf("无法创建输出目录: %v", err)
+	}
+
+	//// 打开 PDF 文件
+	//pdfFile, err := os.Open(inputPDF)
+	//if err != nil {
+	//	log.Fatalf("无法打开 PDF 文件: %v", err)
+	//}
+	//defer pdfFile.Close()
+
+	// 提取图像
+	err = api.ExtractImagesFile(inputPDF, outputDir, []string{"1", "2"}, nil)
+	if err != nil {
+		log.Fatalf("提取图像失败: %v", err)
+	}
+
+	fmt.Printf("图像提取完成，保存在目录: %s\n", outputDir)
 }
