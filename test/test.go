b04380ec87e2585d743a6dd3185959c2d558594d
Richelieu <richelieu042@gmail.com>
2024-08-19 15:42:16 +0800

    init

--- test/test.go
+++ test/test.go
@@ -1,23 +1,5 @@
 package main
 
-import (
-	"fmt"
-	"github.com/pdfcpu/pdfcpu/pkg/api"
-	"log"
-)
-
-func mergePDFs(inputFiles []string, outputFile string) error {
-	return api.MergeCreateFile(inputFiles, outputFile, false, nil)
-}
-
 func main() {
-	files := []string{"/Users/richelieu/Desktop/a.pdf", "/Users/richelieu/Desktop/b.pdf"}
-	output := "merged1.pdf"
 
-	err := mergePDFs(files, output)
-	if err != nil {
-		log.Fatalf("Error merging PDFs: %v", err)
-	}
-
-	fmt.Println("PDFs merged successfully!")
 }
