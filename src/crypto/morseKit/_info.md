00576e58eea05412c7a48fff71652cde1aa0eee1
Richelieu <richelieu042@gmail.com>
2024-08-28 09:21:27 +0800

    init

--- src/crypto/morseKit/_info.md
+++ src/crypto/morseKit/_info.md
@@ -2,4 +2,8 @@
 摩尔斯电码只能表示字母（不区分大小写）、数字和标点符号，对于中文需要一次中间编码转换，这就是中文电码。
 
 ## 摩尔斯电码在线加密解密工具
-https://qqe2.com/ed/morse
+- https://qqe2.com/ed/morse
+- https://www.iamwawa.cn/morse.html
+- https://www.xuhuhu.com/beautify/morse/#google_vignette
+
+
--- test/test.go
+++ test/test.go
@@ -1,13 +1,50 @@
 package main
 
 import (
-	"github.com/richelieu-yang/chimera/v3/src/office/pdfKit"
+	"strings"
 )
 
-func main() {
-	inputPDF := "/Users/richelieu/Desktop/34页(1).pdf"
-	outputDir := "_tmp"
-	if err := pdfKit.SplitByPageNrFile(inputPDF, outputDir, []int{2}, nil); err != nil {
-		panic(err)
+var morseCodeMap = map[rune]string{
+	'A': ".-", 'B': "-...", 'C': "-.-.", 'D': "-..", 'E': ".", 'F': "..-.", 'G': "--.", 'H': "....", 'I': "..", 'J': ".---", 'K': "-.-", 'L': ".-..", 'M': "--", 'N': "-.", 'O': "---", 'P': ".--.", 'Q': "--.-", 'R': ".-.", 'S': "...", 'T': "-", 'U': "..-", 'V': "...-", 'W': ".--", 'X': "-..-", 'Y': "-.--", 'Z': "--..",
+	'1': ".----", '2': "..---", '3': "...--", '4': "....-", '5': ".....", '6': "-....", '7': "--...", '8': "---..", '9': "----.", '0': "-----",
+	' ': "/",
+}
+
+var reverseMorseCodeMap = map[string]rune{}
+
+func init() {
+	for char, code := range morseCodeMap {
+		reverseMorseCodeMap[code] = char
 	}
+}
+
+func encrypt(text string) string {
+	var result strings.Builder
+	for _, char := range text {
+		if code, ok := morseCodeMap[char]; ok {
+			result.WriteString(code)
+			result.WriteString(" ")
+		}
+	}
+	return result.String()
+}
+
+func decrypt(morseText string) string {
+	words := strings.Fields(morseText)
+	var result strings.Builder
+	for _, word := range words {
+		if char, ok := reverseMorseCodeMap[word]; ok {
+			result.WriteRune(char)
+		}
+	}
+	return result.String()
+}
+
+func main() {
+	originalText := "你好，世界！"
+	encrypted := encrypt(originalText)
+	decrypted := decrypt(encrypted)
+	println("原始文本：", originalText)
+	println("加密后的摩尔斯密码：", encrypted)
+	println("解密后的文本：", decrypted)
 }
