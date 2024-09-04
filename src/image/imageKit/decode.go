663c03fae5966c539688c64d3380f2ae8c90ed6f
Richelieu <richelieu042@gmail.com>
2024-09-04 14:03:06 +0800

    init

--- src/image/imageKit/decode.go
+++ src/image/imageKit/decode.go
@@ -10,8 +10,11 @@ import (
 // Decode 解码图片
 /*
 @param r 类型可以是: *os.File（用完记得调用Close()）
+@return 第1个: image.Image实例
+		第2个: 表示图像的格式名称，例如 "png"、"jpeg" 等（不带"." && 转为小写）
+		第3个: error（可能为nil）
 */
-var Decode func(r io.Reader) (image.Image, string, error) = image.Decode
+var Decode func(r io.Reader) (img image.Image, format string, err error) = image.Decode
 
 // DecodeWithPath 解码图片
 func DecodeWithPath(src string) (image.Image, string, error) {
--- test/test.go
+++ test/test.go
@@ -1,50 +1,24 @@
 package main
 
 import (
-	"strings"
+	"fmt"
+	"github.com/richelieu-yang/chimera/v3/src/image/imageKit"
+	"os"
 )
 
-var morseCodeMap = map[rune]string{
-	'A': ".-", 'B': "-...", 'C': "-.-.", 'D': "-..", 'E': ".", 'F': "..-.", 'G': "--.", 'H': "....", 'I': "..", 'J': ".---", 'K': "-.-", 'L': ".-..", 'M': "--", 'N': "-.", 'O': "---", 'P': ".--.", 'Q': "--.-", 'R': ".-.", 'S': "...", 'T': "-", 'U': "..-", 'V': "...-", 'W': ".--", 'X': "-..-", 'Y': "-.--", 'Z': "--..",
-	'1': ".----", '2': "..---", '3': "...--", '4': "....-", '5': ".....", '6': "-....", '7': "--...", '8': "---..", '9': "----.", '0': "-----",
-	' ': "/",
-}
-
-var reverseMorseCodeMap = map[string]rune{}
-
-func init() {
-	for char, code := range morseCodeMap {
-		reverseMorseCodeMap[code] = char
-	}
-}
-
-func encrypt(text string) string {
-	var result strings.Builder
-	for _, char := range text {
-		if code, ok := morseCodeMap[char]; ok {
-			result.WriteString(code)
-			result.WriteString(" ")
-		}
-	}
-	return result.String()
-}
-
-func decrypt(morseText string) string {
-	words := strings.Fields(morseText)
-	var result strings.Builder
-	for _, word := range words {
-		if char, ok := reverseMorseCodeMap[word]; ok {
-			result.WriteRune(char)
-		}
-	}
-	return result.String()
-}
-
 func main() {
-	originalText := "你好，世界！"
-	encrypted := encrypt(originalText)
-	decrypted := decrypt(encrypted)
-	println("原始文本：", originalText)
-	println("加密后的摩尔斯密码：", encrypted)
-	println("解密后的文本：", decrypted)
+	path := "/Users/richelieu/Desktop/iShot_2024-09-04_13.51.58.PNG"
+
+	f, err := os.Open(path)
+	if err != nil {
+		panic(err)
+	}
+	defer f.Close()
+
+	img, tmp, err := imageKit.Decode(f)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(tmp)
+	fmt.Println(img.Bounds().String())
 }
