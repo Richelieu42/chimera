fb49dda0b0c2b1b499737ecf1a101ade8185d45e
Richelieu <richelieu042@gmail.com>
2024-09-11 11:13:14 +0800

    init

--- src/image/qrCodeKit/yeqown_file_test.go
+++ src/image/qrCodeKit/yeqown_file_test.go
@@ -1,6 +1,7 @@
 package qrCodeKit
 
 import (
+	"github.com/richelieu-yang/chimera/v3/src/image/imageKit"
 	"github.com/yeqown/go-qrcode/writer/standard"
 	"testing"
 )
@@ -12,8 +13,14 @@ func TestEncodeToFile(t *testing.T) {
 	}
 }
 
+// 带 logo 的二维码（注意，logo的尺寸不能大于二维码的1/5）.
 func TestEncodeToFile1(t *testing.T) {
-	err := EncodeToFile("https://www.example.com", "_test1.png", standard.WithQRWidth(42))
+	img, _, err := imageKit.DecodeWithImagePath("_logo.png")
+	if err != nil {
+		panic(err)
+	}
+
+	err = EncodeToFile("https://www.example.com", "_test1.png", standard.WithLogoSizeMultiplier(3), standard.WithLogoImage(img))
 	if err != nil {
 		panic(err)
 	}
