5f1b47eb0245429556c4cdef48316331113b22d0
Richelieu <richelieu042@gmail.com>
2024-10-25 13:53:28 +0800

    init

--- test/test.go
+++ test/test.go
@@ -1,30 +1,12 @@
 package main
 
 import (
-	"github.com/gin-gonic/gin"
-	"github.com/richelieu-yang/chimera/v3/src/component/web/ginKit"
-	"github.com/richelieu-yang/chimera/v3/src/config/viperKit"
+	"github.com/richelieu-yang/chimera/v3/src/log/console"
 )
 
 func main() {
-	type config struct {
-		Gin *ginKit.Config `json:"gin" yaml:"gin"`
-	}
-
-	path := "_chimera-lib/config.yaml"
-	c := &config{}
-	//err := yamlKit.UnmarshalFromFile(path, c)
-	_, err := viperKit.UnmarshalFromFile(path, nil, c)
-	if err != nil {
-		panic(err)
-	}
-
-	ginKit.MustSetUp(c.Gin, func(engine *gin.Engine) error {
-		engine.Any("/test", func(ctx *gin.Context) {
-			ctx.String(200, "ok")
-			return
-		})
-
-		return nil
-	}, ginKit.WithServiceInfo("TEST"), ginKit.WithDefaultFavicon(true), ginKit.WithDefaultNoRouteHtml(true))
+	console.Info("info")
+	console.Info("warn")
+	console.Fatal("fatal")
+	console.Error("error")
 }
