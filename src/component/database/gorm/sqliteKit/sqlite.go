06daadd408913a3681d1a515ce3da91beedf59ab
Richelieu <richelieu042@gmail.com>
2024-11-13 14:04:24 +0800

    init

---
+++ src/component/database/gorm/sqliteKit/sqlite.go
@@ -0,0 +1,16 @@
+package sqliteKit
+
+import (
+	"github.com/richelieu-yang/chimera/v3/src/component/database/gorm/gormKit"
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
+)
+
+// NewGormDB
+/*
+@param dsn 详见: notes/Golang/database/gorm.wps
+*/
+func NewGormDB(dsn string, opts ...gorm.Option) (*gorm.DB, error) {
+	dialector := sqlite.Open(dsn)
+	return gormKit.NewDB(dialector, opts...)
+}
