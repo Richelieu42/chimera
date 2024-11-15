9403150f0cf04b45e6670ee9f65d85270c007c94
Richelieu <richelieu042@gmail.com>
2024-11-15 16:34:06 +0800

    init

--- src/file/fileKit/permission.go
+++ src/file/fileKit/permission.go
@@ -11,9 +11,15 @@ const (
 
 	// SafePerm 文件拥有者可以读写文件，而组用户和其他用户只能读取文件.
 	/*
-		4: 读权限
-		2: 写权限
-		1: 执行权限
+		3种权限:
+		(1) 4: 读权限
+		(2) 2: 写权限
+		(3) 1: 执行权限
+
+		第1位: 表示特殊权限位（不常用，通常为 0）
+		第2位: 表示文件所有者的权限
+		第3位: 表示所属组的权限
+		第4位: 表示其他用户的权限
 	*/
 	SafePerm os.FileMode = 0644
 )
@@ -23,8 +29,7 @@ const (
 	@param path 文件（或目录）的路径
 	@return 传参path不存在的话，将返回false
 
-	e.g.
-		("") => false
+	e.g. ("") => false
 */
 func IsReadable(path string) bool {
 	return gfile.IsReadable(path)
@@ -35,8 +40,7 @@ func IsReadable(path string) bool {
 	@param path 文件（或目录）的路径
 	@return 传参path不存在的话，将返回false
 
-	e.g.
-		("") => false
+	e.g. ("") => false
 */
 func IsWritable(path string) bool {
 	return gfile.IsWritable(path)
