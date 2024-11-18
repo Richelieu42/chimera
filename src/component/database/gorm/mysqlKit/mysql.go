9a6e83bbc8392f379ef91729b88908052320ddc6
Richelieu <richelieu042@gmail.com>
2024-11-18 11:09:30 +0800

    init

--- src/component/database/gorm/mysqlKit/mysql.go
+++ src/component/database/gorm/mysqlKit/mysql.go
@@ -11,12 +11,11 @@ import (
 /*
 参考: https://gorm.io/zh_CN/docs/connecting_to_the_database.html#MySQL
 
-PS:
-(1) charset=utf8mb4:	指定字符集为 utf8mb4，适用于支持表情符号等扩展字符集的场景。
-(2) parseTime=True: 	表示将数据库中的时间值解析为 Go 的 time.Time 类型。
-(3) loc=Local:	 		指定时区为本地时区，适用于数据库时间和本地时间保持一致的情况。
-
 @param dsn	"user:pass@tcp(127.0.0.1:3306)/dbname?charset=utf8mb4&parseTime=True&loc=Local"
+			查询参数:
+			(1) charset=utf8mb4:	指定字符集为 utf8mb4，适用于支持表情符号等扩展字符集的场景。
+			(2) parseTime=True: 	表示将数据库中的时间值解析为 Go 的 time.Time 类型。
+			(3) loc=Local:	 		指定时区为本地时区，适用于数据库时间和本地时间保持一致的情况。
 @param opts	可以不传
 */
 func NewGormDB(dsn string, opts ...gorm.Option) (*gorm.DB, error) {
