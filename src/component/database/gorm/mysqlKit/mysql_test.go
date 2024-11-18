531859eb440c51fb3d98d41eccca7ba7e883a207
Richelieu <richelieu042@gmail.com>
2024-11-18 13:38:49 +0800

    init

--- src/component/database/gorm/mysqlKit/mysql_test.go
+++ src/component/database/gorm/mysqlKit/mysql_test.go
@@ -6,8 +6,8 @@ import (
 )
 
 func TestNewGormDB(t *testing.T) {
-	user := "yjs"
-	password := "Test123/@"
+	user := "test"
+	password := "测试test123"
 	dsn := fmt.Sprintf("%s:%s@tcp(101.32.170.155:3306)/mysql?charset=utf8mb4&parseTime=True&loc=Local", user, password)
 	fmt.Printf("dsn: %s\n", dsn)
 
